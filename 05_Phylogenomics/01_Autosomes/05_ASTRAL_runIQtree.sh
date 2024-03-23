acd65d4b6b4599486c01583d99258834502fc83d
Lauren Hennelly <lmhennelly@ucdavis.edu>
2024-03-23 13:00:24 -0400

    Create 05_ASTRAL_runIQtree.sh

---
+++ 05_Phylogenomics/01_Autosomes/05_ASTRAL_runIQtree.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+#SBATCH --job-name=ASTRAL
+#SBATCH -c 1
+#SBATCH --time 01:40:00
+#SBATCH --mem-per-cpu 1G
+#SBATCH -o /home/crq857/projects/Geneflow_Dogs/slurmout/iqtree_fullgenome_run_%A_%a.out
+#SBATCH -e /home/crq857/projects/Geneflow_Dogs/slurmout/iqtree_fullgenome_run_%A_%a.err
+#SBATCH --array=1-5000%10
+
+#conda activate /projects/mjolnir1/apps/conda/python-3.5.6
+
+PHY=$(sed "${SLURM_ARRAY_TASK_ID}q;d" /projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/phyfiles/filelist.txt)
+
+/home/crq857/bin/iqtree-1.6.12-Linux/bin/iqtree -s  /projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/phyfiles/${PHY} -bb 1000 -nt AUTO
