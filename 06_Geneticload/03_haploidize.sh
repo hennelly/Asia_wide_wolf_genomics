3b97dd5ee5044f31b851a4aeccf59ec61a846f8d
Lauren Hennelly <lmhennelly@ucdavis.edu>
2024-04-05 16:06:47 -0400

    Create 03_haploidize.sh

---
+++ 06_Geneticload/03_haploidize.sh
@@ -0,0 +1,5 @@
+
+## pseudo haploidize vcf files (i had to create a python virtual environment with specific dependencies but might just be our server) ##
+python3 pseudohap.py -v [outgroup]_test.vcf -d 5 -o [outgroup]_pseudo
+
+
