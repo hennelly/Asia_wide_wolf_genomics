7c4ef37239df1dfaeb488f665095be55e840939e
Lauren Hennelly <lmhennelly@ucdavis.edu>
2024-04-05 15:50:04 -0400

    Update 02_callancestralallele.sh

--- 06_Geneticload/02_callancestralallele.sh
+++ 06_Geneticload/02_callancestralallele.sh
@@ -1,15 +1,36 @@
+#!/usr/bin/env bash
+#SBATCH --job-name=gload
+#SBATCH -c 1
+#SBATCH --time 00:30:00
+#SBATCH --mem-per-cpu 1G
+#SBATCH -o /home/crq857/projects/reproductivegenes/slurmout/extract.out
+#SBATCH -e /home/crq857/projects/reproductivegenes/slurmout/extract.err
+
+module load perl
+module load vcftools 
+
+SNPvcf=/projects/mjolnir1/people/crq857/Chapter2/00_Alignment/05_GenotypeGATK/Autosomes_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9_geneticload_WOLVES_ID_noheader.vcf
+VCF=/projects/mjolnir1/people/crq857/Chapter2/00_Alignment/05_GenotypeGATK/Autosomes_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9_geneticload_ID.vcf
+OUT=/projects/mjolnir1/people/crq857/Chapter2/00_Alignment/05_GenotypeGATK/Autosomes_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9_geneticload_ancetral
+
+#create snplist
+awk '{ print $3 }' ${SNPvcf} > /projects/mjolnir1/people/crq857/Chapter2/00_Alignment/05_GenotypeGATK/snp.list
+
+SNPS=/projects/mjolnir1/people/crq857/Chapter2/00_Alignment/05_GenotypeGATK/snp.list
+OUTd=/projects/mjolnir1/people/crq857/Chapter2/00_Alignment/05_GenotypeGATK/Dhole
+OUTa=/projects/mjolnir1/people/crq857/Chapter2/00_Alignment/05_GenotypeGATK/Andeanfox
+
+#keep only snps in focal wolf pop
+vcftools --vcf ${VCF} --snps ${SNPS} --recode --recode-INFO-all --out ${OUT}
+
+#vcf for Dhole
+vcftools --vcf ${OUT}.recode.vcf --indv Dhole_BerlinZoo --recode --recode-INFO-all --out ${OUTd}
+
+#vcf for Andean fox 
+vcftools --vcf ${OUT}.recode.vcf --indv AndeanFox --recode --recode-INFO-all --out ${OUTa}
 
 
-# use original file 
-VCF=/projects/mjolnir1/people/crq857/Chapter2/00_Alignment/05_GenotypeGATK/Autosomes_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9.recode.vcf.vcf
-# Get snplist from focal populations 
-OUT=/projects/mjolnir1/people/crq857/Chapter2/00_Alignment/05_GenotypeGATK/Autosomes_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9_geneticload
-
-# Subset original file to only the sites within the focal population vcf ${OUT}
-# pull out each individual outgroup to its own vcf 
-# use that to psueodohaploidize 
 
 
-# create a SNP list 
-awk '{ print $3 }' ${VCF} > snp.list
+
 
