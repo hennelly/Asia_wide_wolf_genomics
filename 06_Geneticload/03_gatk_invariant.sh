8bdace6e385eb6bdbbf120c95e5e2995d127c19d
Lauren Hennelly <lmhennelly@ucdavis.edu>
2024-04-08 23:38:07 -0400

    Update 03_gatk_invariant.sh

--- 06_Geneticload/03_gatk_invariant.sh
+++ 06_Geneticload/03_gatk_invariant.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 #SBATCH --job-name=d
 #SBATCH -c 1
-#SBATCH --time 1-00:10:00
-#SBATCH --mem-per-cpu 20G
+#SBATCH --time 30:00
+#SBATCH --mem-per-cpu 1G
 #SBATCH -o /home/crq857/projects/Chapter2/slurmout_fsc/andeanfsc_GATK_Genotype.out
 #SBATCH -e /home/crq857/projects/Chapter2/slurmout_fsc/andeanfsc_GATK_Genotype.err
 
@@ -19,6 +19,63 @@ java -Xmx20g -jar /projects/mjolnir1/apps/conda/gatk4-4.1.2.0/share/gatk4-4.1.2.
    -ERC BP_RESOLUTION \
    --output-mode EMIT_ALL_CONFIDENT_SITES \
    -L /projects/mjolnir1/people/crq857/Chapter2/00_Alignment/05_GenotypeGATK/your_file.bed \
+
+
+#!/usr/bin/env bash
+#SBATCH --job-name=Combine_VCF
+#SBATCH --nodes 1
+#SBATCH --ntasks 1
+#SBATCH --time 06:00:00
+#SBATCH --mem=16GB
+#SBATCH -p high
+#SBATCH -o arrayJob_%A_%a.out
+#SBATCH --exclude=c10-96,c10-69,c11-76,c11-93,c11-86
+
+cat <(grep "#" chr1/freebayes/chr1.1000001-2000001.vcf) <(for number in $(seq 1 1 39); do grep -v "#" chr${number}/chr${number}_minQ30_noindels.recode.vcf; done) <(grep -v "#" chrX/chrX_minQ30_noindels.recode.vcf) > All_Combined_minQ30_noindels.vcf
+
+#!/usr/bin/env bash
+#SBATCH --job-name=fsc
+#SBATCH -c 1
+#SBATCH --time 00:30:00
+#SBATCH --mem-per-cpu 1G
+#SBATCH -o /home/crq857/projects/Chapter2/slurmout/fsc_GATK_Genotype_3.out
+#SBATCH -e /home/crq857/projects/Chapter2/slurmout/fsc_GATK_Genotype_3.err
+
+cat <(grep "#" /projects/mjolnir1/people/crq857/Chapter2/00_Alignment/05_BCFtools_fsc/chr1/samtools/chr1.1000001-2000001.vcf) \
+        <(for number in $(seq 1 1 39); do grep -v "#" /projects/mjolnir1/people/crq857/Chapter2/00_Alignment/05_BCFtools_fsc/chr${number}/chr${number}.samtools.WithIDs.vcf | awk '$5!="." {print}'; done) > /projects/mjolnir1/people/crq857/Chapter2/00_Alignment/05_BCFtools_fsc/All_Combined.samtools.WithIDs.vcf
+
+
+
+#!/usr/bin/env bash
+#SBATCH --job-name=d
+#SBATCH -c 1
+#SBATCH --time 00:30:00
+#SBATCH --mem-per-cpu 1G
+#SBATCH -o gun2zip.out
+#SBATCH -e gunz2ip.err
+
+gunzip *vcf.gz
+
+#!/usr/bin/env bash
+#SBATCH --job-name=gload
+#SBATCH -c 1
+#SBATCH --time 04:00:00
+#SBATCH --mem-per-cpu 1G
+#SBATCH -o /home/crq857/projects/reproductivegenes/slurmout/hapdAndeanfox.out
+#SBATCH -e /home/crq857/projects/reproductivegenes/slurmout/hapdAndeanfox.err
+
+## pseudo haploidize vcf files (i had to create a python virtual environment with specific dependencies but might just be our server) ##
+gunzip /projects/mjolnir1/people/crq857/Chapter2/00_Alignment/05_GenotypeGATK_fsc/Andeanfox.vcf.gz
+
+VCFd=/projects/mjolnir1/people/crq857/Chapter2/00_Alignment/05_GenotypeGATK_fsc/Andeanfox.vcf.gz
+OUTd=/projects/mjolnir1/people/crq857/Chapter2/00_Alignment/05_GenotypeGATK/Andeanfox_pseudo_gatk
+
+python3 pseudo_haploidize.py -v ${VCFd} -d 4 -o ${OUTd}
+
+
+gunzip /projects/mjolnir1/people/crq857/Chapter2/00_Alignment/05_GenotypeGATK_fsc/Dhole.vcf.gz
+
+/projects/mjolnir1/people/crq857/Chapter2/00_Alignment/05_GenotypeGATK_fsc/Dhole.vcf.gz
 
 
 
