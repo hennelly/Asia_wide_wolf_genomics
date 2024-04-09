52f307412c74478d73a0daee89212f0963d6b253
Lauren Hennelly <lmhennelly@ucdavis.edu>
2024-04-09 13:54:09 -0400

    Update 04_haploidize.sh

--- 06_Geneticload/04_haploidize.sh
+++ 06_Geneticload/04_haploidize.sh
@@ -16,5 +16,16 @@ VCFa=/projects/mjolnir1/people/crq857/Chapter2/00_Alignment/05_GenotypeGATK_fsc/
 OUTa=/projects/mjolnir1/people/crq857/Chapter2/00_Alignment/05_GenotypeGATK/Andeanfox_pseudo_gatk
 
 python3 pseudo_haploidize.py -v ${VCFd} -d 4 -o ${OUTd}
-
 python3 pseudo_haploidize.py -v ${VCFa} -d 4 -o ${OUTa}
+
+
+dp10_filtered_indDP_mean_min_max.txt
+
+dp10_filtered_indDP.vcf.gz' is not BGZF
+
+
+Rscript Rscripts/getMeanLimitsDP.r dp10_filtered popfile.txt dp10_filtered_indsVCF.txt 0.5 2 dp10_filtered_indDP_mean_min_max.txt ;
+# this script outputs a file with the median, min and max DP per individual with the following file name
+indthresholdDP="dp10_filtered_indDP_mean_min_max.txt";
+
+
