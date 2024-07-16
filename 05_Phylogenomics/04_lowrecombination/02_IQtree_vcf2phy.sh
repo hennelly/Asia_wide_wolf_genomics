481178c2a8384f96dca37848fcdd2368a82e22d3
Lauren Hennelly <lmhennelly@ucdavis.edu>
2024-07-16 18:21:22 -0400

    Update 02_IQtree_vcf2phy.sh

--- 05_Phylogenomics/04_lowrecombination/02_IQtree_vcf2phy.sh
+++ 05_Phylogenomics/04_lowrecombination/02_IQtree_vcf2phy.sh
@@ -3,18 +3,22 @@
 #SBATCH -c 1
 #SBATCH --time 00:40:00
 #SBATCH --mem-per-cpu 1G
-#SBATCH -o /home/crq857/projects/Geneflow_Dogs/slurmout/vcf2python_fullgenome.out
-#SBATCH -e /home/crq857/projects/Geneflow_Dogs/slurmout/vcf2python_fullgenome.err
+#SBATCH -o /home/crq857/projects/Chapter2/slurmout/lowrecombination_vcf2phy.out
+#SBATCH -e /home/crq857/projects/Chapter2/slurmout/lowrecombination_vcf2phy.err
 
 #conda activate /projects/mjolnir1/apps/conda/python-3.5.6 #this may differ for your cluster. The vcf2phylip needs a specific python version
 
-#autosomes
-VCF=/projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/input
-OUTDIR=/projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/phyfiles
+#Xchromosome
+XVCF=/projects/mjolnir1/people/crq857/Chapter2/06_Datasets/Xchr_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9_forASTRAl_lowrecombination.vcf.recode.vcf
+XOUTDIR=/projects/mjolnir1/people/crq857/Chapter2/06_Datasets/
 
-python /projects/mjolnir1/people/crq857/Geneflow_Dogs/bin/vcf2phylip/vcf2phylip.py -i ${DIR}/autosomes.vcf --output-folder ${OUTDIR} --output-prefix autosomes_phy -o Dhole_BerlinZoo
+python /projects/mjolnir1/people/crq857/Geneflow_Dogs/bin/vcf2phylip/vcf2phylip.py -i ${VCF} --output-folder ${OUTDIR} --output-prefix Xchr_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9_forASTRAl_lowrecombination -o Dhole_BerlinZoo
 
 #You can download vcf2phylip here: https://github.com/edgardomortiz/vcf2phylip
 # In this script above, you also need to indicate an outgroup with the -o flag
 
-#X chromosome 
+#Autosomes 
+VCF=/projects/mjolnir1/people/crq857/Chapter2/06_Datasets/Autosomes_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9_forASTRAL_lowrecombination.vcf.recode.vcf
+OUTDIR=/projects/mjolnir1/people/crq857/Chapter2/06_Datasets/
+
+python /projects/mjolnir1/people/crq857/Geneflow_Dogs/bin/vcf2phylip/vcf2phylip.py -i ${VCF} --output-folder ${OUTDIR} --output-prefix Autosomes_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9_forASTRAl_lowrecombination -o Dhole_BerlinZoo
