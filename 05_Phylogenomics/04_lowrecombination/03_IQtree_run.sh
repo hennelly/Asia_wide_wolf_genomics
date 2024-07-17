d6ca5f678879ab8db74c25b7586be322fb7666b4
Lauren Hennelly <lmhennelly@ucdavis.edu>
2024-07-17 11:46:15 -0400

    Update 03_IQtree_run.sh

--- 05_Phylogenomics/04_lowrecombination/03_IQtree_run.sh
+++ 05_Phylogenomics/04_lowrecombination/03_IQtree_run.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 #SBATCH --job-name=lworec
 #SBATCH -c 1
-#SBATCH --time 11:40:00
-#SBATCH --mem-per-cpu 1G
-#SBATCH -o /home/crq857/projects/Chapter2/slurmout/iqtree_Xchr_noadmix.out
-#SBATCH -e /home/crq857/projects/Chapter2/slurmout/iqtree_Xchr_noadmix.err
+#SBATCH --time 6-11:40:00
+#SBATCH --mem-per-cpu 60G
+#SBATCH -o /home/crq857/projects/Chapter2/slurmout/iqtree_Auto_noadmix.out
+#SBATCH -e /home/crq857/projects/Chapter2/slurmout/iqtree_Auto_noadmix.err
 
 #conda activate /projects/mjolnir1/apps/conda/python-3.5.6
 
@@ -12,6 +12,10 @@ PHY=/projects/mjolnir1/people/crq857/Chapter2/06_Datasets/Xchr_filtered_noindels
 PHYnoad=/projects/mjolnir1/people/crq857/Chapter2/06_Datasets/Xchr_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9_forASTRAl_lowrecombination_noadmixPak.min4.phy
 /home/crq857/bin/iqtree-1.6.12-Linux/bin/iqtree -s ${PHY} -bb 1000 -nt AUTO -m MFP
 /home/crq857/bin/iqtree-1.6.12-Linux/bin/iqtree -s ${PHYnoad} -bb 1000 -nt AUTO -m MFP
+
+PHY_auto_noadmi=/projects/mjolnir1/people/crq857/Chapter2/06_Datasets/Autosomes_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9_forASTRAl_lowrecombination_noadmix_Pak.min4.phy
+/home/crq857/bin/iqtree-1.6.12-Linux/bin/iqtree -s ${PHY_auto_noadmi} -bb 1000 -nt AUTO -m MFP
+
 
 # you may need to download iqtree: http://www.iqtree.org/
 # the -m flag is to decide which substitution model best fits the data. The MFP flag is telling iqtree to estimate which model is best
