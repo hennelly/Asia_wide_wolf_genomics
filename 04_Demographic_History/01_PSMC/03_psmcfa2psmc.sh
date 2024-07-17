dbd9480e727e168fc0bd60e51c89914fdc8871a6
Lauren Hennelly <lmhennelly@ucdavis.edu>
2024-07-17 14:32:15 -0400

    Update 03_psmcfa2psmc.sh

--- 04_Demographic_History/01_PSMC/03_psmcfa2psmc.sh
+++ 04_Demographic_History/01_PSMC/03_psmcfa2psmc.sh
@@ -3,40 +3,22 @@
 #SBATCH -c 1
 #SBATCH --time 12:00:00
 #SBATCH --mem-per-cpu 5G
-#SBATCH --array=1-15%10
+#SBATCH --array=1-23%10
 #SBATCH -o /home/crq857/projects/Chapter2/slurmout/PSMC_psmc_rerun_testlc_%A_%a.out
 #SBATCH -e /home/crq857/projects/Chapter2/slurmout/PSMC_psmc_rerun_testlc_%A_%a.err
 
 echo "My SLURM_ARRAY_TASK_ID: " $SLURM_ARRAY_TASK_ID
-PSMCFA=$(sed "${SLURM_ARRAY_TASK_ID}q;d" /home/crq857/projects/Chapter2/list_Dec/psmc_lc_list.txt | cut -f1)
+PSMCFA=$(sed "${SLURM_ARRAY_TASK_ID}q;d" /home/crq857/projects/Chapter2/list_Dec/psmcfa_highcov.txt | cut -f1)
 DIR=/projects/mjolnir1/people/crq857/Chapter2/04_Demographichistory/PSMC/psmcfa
 OUTDIR=/projects/mjolnir1/people/crq857/Chapter2/04_Demographichistory/PSMC/psmc
 
 module load psmc
 
-psmc -p "4+25*2+4+6" -r5 -t15 -N25 -o ${OUTDIR}/${PSMCFA}_4.psmc ${DIR}/${PSMCFA}.psmcfa
+psmc -p "4+25*2+4+6" -r5 -t15 -N25 -o ${OUTDIR}/${PSMCFA}_4.psmc ${DIR}/${PSMCFA}
 
 ## Also try other combinations of input to see if it impacts the PSMC curve 
 
-psmc -p "2+2+25*2+4+6" -r5 -t15 -N25 -o ${OUTDIR}/${PSMCFA}_2_2.psmc ${DIR}/${PSMCFA}.psmcfa
-psmc -p "1+1+1+1+25*2+4+6" -r5 -t15 -N25 -o ${OUTDIR}/${PSMCFA}_1_1_1_1.psmc ${DIR}/${PSMCFA}.psmcfa
+psmc -p "2+2+25*2+4+6" -r5 -t15 -N25 -o ${OUTDIR}/${PSMCFA}_2_2.psmc ${DIR}/${PSMCFA}
+psmc -p "1+1+1+1+25*2+4+6" -r5 -t15 -N25 -o ${OUTDIR}/${PSMCFA}_1_1_1_1.psmc ${DIR}/${PSMCFA}
 
 
-
-
-#!/usr/bin/env bash
-#SBATCH --job-name=PSMC
-#SBATCH -c 1
-#SBATCH --time 12:00:00
-#SBATCH --mem-per-cpu 1G
-#SBATCH -o /home/crq857/projects/Chapter2/slurmout/PSMC_psmc_BH123_correction.out
-#SBATCH -e /home/crq857/projects/Chapter2/slurmout/PSMC_psmc_BH123_correction.err
-
-DIR=/projects/mjolnir1/people/crq857/Chapter2/04_Demographichistory/PSMC/psmcfa
-OUTDIR=/projects/mjolnir1/people/crq857/Chapter2/04_Demographichistory/PSMC/psmc
-
-module load psmc
-
-psmc -p "4+25*2+4+6" -r5 -t15 -N25 -o ${OUTDIR}/Indianwolf_BH123_final_autosomes_4.psmc ${DIR}/Indianwolf_BH123_final_autosomes.psmcfa
-psmc -p "2+2+25*2+4+6" -r5 -t15 -N25 -o ${OUTDIR}/Indianwolf_BH123_final_autosomes_2_2.psmc ${DIR}/Indianwolf_BH123_final_autosomes.psmcfa
-psmc -p "1+1+1+1+25*2+4+6" -r5 -t15 -N25 -o ${OUTDIR}/Indianwolf_BH123_final_autosomes_1_1_1_1.psmc ${DIR}/Indianwolf_BH123_final_autosomes.psmcfa
