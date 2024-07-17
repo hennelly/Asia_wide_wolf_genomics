0fbd6d025503ece4ae246edb30c73309a030a8c5
Lauren Hennelly <lmhennelly@ucdavis.edu>
2024-07-17 10:35:08 -0400

    Create 04_filter_noadmix.sh

---
+++ 05_Phylogenomics/04_lowrecombination/04_filter_noadmix.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+#SBATCH --job-name=lowrec
+#SBATCH -c 1
+#SBATCH --time 03:30:00
+#SBATCH --mem-per-cpu 1G
+#SBATCH -o /home/crq857/projects/Chapter2/slurmout/lowrecombination_removeadmix.out
+#SBATCH -e /home/crq857/projects/Chapter2/slurmout/lowrecombination_removeadmix.err
+
+XVCF=/projects/mjolnir1/people/crq857/Chapter2/06_Datasets/Xchr_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9_forASTRAl_lowrecombination.vcf.recode.vcf
+VCF=/projects/mjolnir1/people/crq857/Chapter2/06_Datasets/Autosomes_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9_forASTRAL_lowrecombination.vcf.recode.vcf
+REMOVE=/home/crq857/projects/Chapter2/files/removeadmix.txt
+XOUTVCF=/projects/mjolnir1/people/crq857/Chapter2/06_Datasets/Xchr_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9_forASTRAl_lowrecombination_noPakistan
+OUTVCF=/projects/mjolnir1/people/crq857/Chapter2/06_Datasets/Autosomes_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9_forASTRAl_lowrecombination_noPakistan
+
+module load perl
+module load vcftools
+
+vcftools --vcf ${XVCF} --remove ${REMOVE} --out ${XOUTVCF} --recode --keep-INFO-all
+vcftools --vcf ${VCF} --remove ${REMOVE} --out ${OUTVCF} --recode --keep-INFO-all
+
+
+
+
