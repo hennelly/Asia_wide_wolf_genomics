8609f45caa6cbea404671070fdb6bfad0f0a1164
Lauren Hennelly <lmhennelly@ucdavis.edu>
2024-07-17 16:27:47 -0400

    Update 03_twisst_run.sh

--- 03_Geneflow_Analyses/01_twisst/03_twisst_run.sh
+++ 03_Geneflow_Analyses/01_twisst/03_twisst_run.sh
@@ -1,4 +1,4 @@
-q#!/usr/bin/env bash
+#!/usr/bin/env bash
 #SBATCH --job-name=twisst
 #SBATCH -c 1
 #SBATCH --time 1-10:30:00
@@ -17,7 +17,7 @@ module load python/2.7.18
 module load numpy
 module load phyml
 
-python /projects/mjolnir1/people/crq857/bin/twisst/twisst.py -t ${DIR}/gatk_${CHR}_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9.final.phyml.trees.gz -w ${OUTDIR}/gatk_${CHR}_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9.final_twisst -g Africanwolf -g Indianwolf -g WestAsia -g CentralAsia -g Europe -g Andeanfox --method complete --groupsFile ${popfile} 
+python /projects/mjolnir1/people/crq857/bin/twisst/twisst.py -t ${DIR}/gatk_${CHR}_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9.final.phyml.trees.gz -w ${OUTDIR}/gatk_${CHR}_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9.final_twisst -g Africanwolf -g Indianwolf -g WestAsia -g CentralAsia -g Europe  --outgroup Andeanfox --method complete --groupsFile ${popfile} 
 
 
 Twisst_popfile.txt
@@ -29,10 +29,10 @@ Twisst_popfile.txt
 #SBATCH --ntasks 1
 #SBATCH --time 3-12:00:00
 #SBATCH --mem=40GB
-#SBATCH -o /home/hennelly/Asianwolves/slurmout/02_twisst_Asianwolves_final_Tib_nnewtwisst_%A_%a.out
-#SBATCH -e /home/hennelly/Asianwolves/slurmout/02_twisst_Asianwolves_final_Tib_nnewtwisst_%A_%a.err
+#SBATCH -o /home/hennelly/Asianwolves/slurmout/02_twisst_Asianwolves_final_west_nnewtwisst_%A_%a.out
+#SBATCH -e /home/hennelly/Asianwolves/slurmout/02_twisst_Asianwolves_final_west_nnewtwisst_%A_%a.err
 #SBATCH -p high
-#SBATCH --array=1-38
+#SBATCH --array=1-38%15
 
 echo "My SLURM_ARRAY_TASK_ID: " $SLURM_ARRAY_TASK_ID
 CHR=$(sed "${SLURM_ARRAY_TASK_ID}q;d" /home/hennelly/Redwolf/files/chrlist.txt | cut -f1)
@@ -44,11 +44,11 @@ popfile=/group/ctbrowngrp2/hennelly/hennelly/Asianwolves/phyml/Twisst_popfile.tx
 conda activate ete3_python3
 
 #For West Asian wolves
-python3 /home/hennelly/Redwolf/bin/twisst/twisst.py -t ${DIR}/gatk_chr${CHR}_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9.final.phyml.trees.gz -w ${OUTDIR}/gatk_chr${CHR}_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9.final_twisst -g Africanwolf -g Indianwolf -g WestAsia -g CentralAsia -g Europe -g Andeanfox --method complete --groupsFile ${popfile} 
+python3 /home/hennelly/Redwolf/bin/twisst/twisst.py -t ${DIR}/gatk_chr${CHR}_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9.final.phyml.trees.gz -w ${OUTDIR}/gatk_chr${CHR}_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9.final_twisst -g Africanwolf -g Indianwolf -g WestAsia -g CentralAsia -g Europe -g Andeanfox --outgroup Andeanfox --method complete --groupsFile ${popfile} 
 
 popfileTib=/group/ctbrowngrp2/hennelly/hennelly/Asianwolves/phyml/TwisstTibetan_popfile.txt
 #For Tajikistan wolves
-python3 /home/hennelly/Redwolf/bin/twisst/twisst.py -t ${DIR}/gatk_chr${CHR}_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9.final.phyml.trees.gz -w ${OUTDIR}/gatk_chr${CHR}_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9.final_twisst_CentralAsia -g Tibetan -g CentralAsiaMountains -g WestAsia -g Europe -g Andeanfox --method complete --groupsFile ${popfileTib} 
+python3 /home/hennelly/Redwolf/bin/twisst/twisst.py -t ${DIR}/gatk_chr${CHR}_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9.final.phyml.trees.gz -w ${OUTDIR}/gatk_chr${CHR}_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9.final_twisst_CentralAsia -g Tibetan -g CentralAsiaMountains -g WestAsia -g Europe -g Andeanfox --outgroup Andeanfox --method complete --groupsFile ${popfileTib} 
 
 
 
