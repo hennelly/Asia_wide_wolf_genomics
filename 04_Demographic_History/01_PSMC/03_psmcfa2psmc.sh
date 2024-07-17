98221c1283952b0b107c25b6c5f0a2891cb1e9ec
Lauren Hennelly <lmhennelly@ucdavis.edu>
2024-07-16 20:35:02 -0400

    Create 03_psmcfa2psmc.sh

---
+++ 04_Demographic_History/01_PSMC/03_psmcfa2psmc.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+#SBATCH --job-name=PSMC
+#SBATCH -c 1
+#SBATCH --time 12:00:00
+#SBATCH --mem-per-cpu 5G
+#SBATCH --array=1-15%10
+#SBATCH -o /home/crq857/projects/Chapter2/slurmout/PSMC_psmc_rerun_lc_%A_%a.out
+#SBATCH -e /home/crq857/projects/Chapter2/slurmout/PSMC_psmc_rerun_lc_%A_%a.err
+
+echo "My SLURM_ARRAY_TASK_ID: " $SLURM_ARRAY_TASK_ID
+PSMCFA=$(sed "${SLURM_ARRAY_TASK_ID}q;d" /home/crq857/projects/Chapter2/list_Dec/psmc_lc_list.txt | cut -f1)
+DIR=/projects/mjolnir1/people/crq857/Chapter2/04_Demographichistory/PSMC/psmcfa
+OUTDIR=/projects/mjolnir1/people/crq857/Chapter2/04_Demographichistory/PSMC/psmc
+
+module load psmc
+
+psmc -p "4+25*2+4+6" -r5 -t15 -N25 -o ${OUTDIR}/${PSMCFA}.psmc ${DIR}/${PSMCFA}_4.psmcfa
+
+## Also try other combinations of input to see if it impacts the PSMC curve 
+
+psmc -p "2+2+25*2+4+6" -r5 -t15 -N25 -o ${OUTDIR}/${PSMCFA}.psmc ${DIR}/${PSMCFA}_2_2.psmcfa
+psmc -p "1+1+1+1+25*2+4+6" -r5 -t15 -N25 -o ${OUTDIR}/${PSMCFA}.psmc ${DIR}/${PSMCFA}_1_1_1_1.psmcfa
+
