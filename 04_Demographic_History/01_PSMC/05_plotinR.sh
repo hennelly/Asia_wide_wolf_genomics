8c0e071ebfc3df81cc39502628f0a1267116e341
Lauren Hennelly <lmhennelly@ucdavis.edu>
2024-07-17 22:29:22 -0400

    Create 05_plotinR.sh

---
+++ 04_Demographic_History/01_PSMC/05_plotinR.sh
@@ -0,0 +1,3 @@
+
+scp -r crq857@mjolnirgate.unicph.domain:/projects/mjolnir1/people/crq857/Chapter2/04_Demographichistory/PSMC/plot_recheck/*.txt   ~/Desktop
+/projects/mjolnir1/people/crq857/Chapter2/04_Demographichistory/PSMC/plot_recheck/*.txt
