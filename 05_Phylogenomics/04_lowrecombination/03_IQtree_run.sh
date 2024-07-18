cfac04cb2b05683e66cef81cf59be889f35d8fd9
Lauren Hennelly <lmhennelly@ucdavis.edu>
2024-07-18 13:03:33 -0400

    Update 03_IQtree_run.sh

--- 05_Phylogenomics/04_lowrecombination/03_IQtree_run.sh
+++ 05_Phylogenomics/04_lowrecombination/03_IQtree_run.sh
@@ -3,12 +3,12 @@
 #SBATCH -c 1
 #SBATCH --time 6-11:40:00
 #SBATCH --mem-per-cpu 60G
-#SBATCH -o /home/crq857/projects/Chapter2/slurmout/iqtree_Auto_noadmix.out
-#SBATCH -e /home/crq857/projects/Chapter2/slurmout/iqtree_Auto_noadmix.err
+#SBATCH -o /home/crq857/projects/Chapter2/slurmout/iqtree_Auto_noadmix_chrX_WestAsia.out
+#SBATCH -e /home/crq857/projects/Chapter2/slurmout/iqtree_Auto_noadmix_chrX_WestAsia.err
 
 #conda activate /projects/mjolnir1/apps/conda/python-3.5.6
 
-PHY=/projects/mjolnir1/people/crq857/Chapter2/06_Datasets/Xchr_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9_forASTRAl_lowrecombination.min4.phy
+PHY=/projects/mjolnir1/people/crq857/Chapter2/06_Datasets/Xchr_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9_forASTRAl_lowrecombination_noadmixPakWestAsia.min4.phy
 PHYnoad=/projects/mjolnir1/people/crq857/Chapter2/06_Datasets/Xchr_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9_forASTRAl_lowrecombination_noadmixPak.min4.phy
 /home/crq857/bin/iqtree-1.6.12-Linux/bin/iqtree -s ${PHY} -bb 1000 -nt AUTO -m MFP
 /home/crq857/bin/iqtree-1.6.12-Linux/bin/iqtree -s ${PHYnoad} -bb 1000 -nt AUTO -m MFP
