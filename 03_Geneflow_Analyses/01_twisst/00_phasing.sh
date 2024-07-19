4213e98bdf929d1fd32340457a391f0f72f82459
Lauren Hennelly <lmhennelly@ucdavis.edu>
2024-07-19 16:00:19 -0400

    Update 00_phasing.sh

--- 03_Geneflow_Analyses/01_twisst/00_phasing.sh
+++ 03_Geneflow_Analyses/01_twisst/00_phasing.sh
@@ -15,3 +15,11 @@ gunzip ${VCF}.gz
 GENEMAPDIR=/group/ctbrowngrp2/hennelly/hennelly/Pakistan_Graywolf_Genomics/RunsofHomozygosity/ROH_bcftools/ANALYSIS_DEC152021/dog_recomb/canFam3.1/maps
 
 /group/ctbrowngrp2/hennelly/hennelly/Pakistan_Graywolf_Genomics/Selection/Genes/IGF1region/shapeit/shapeit.v2.904.3.10.0-693.11.6.el7.x86_64/bin/shapeit -M ${GENEMAPDIR}/mark4_cleaned_chrX.cf3.1.sortedal2_Apr040.txt --input-vcf ${VCF}  -O /group/ctbrowngrp2/hennelly/hennelly/Redwolf/files_May/Asianwolves_gatk_chrX_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9 --window 0.5 -T 4
+
+#######################
+# Convert haps to vcf #
+#######################
+
+XCHR=/group/ctbrowngrp2/hennelly/hennelly/Redwolf/files_May/Asianwolves_gatk_chrX_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9
+OUTVCF=/group/ctbrowngrp2/hennelly/hennelly/Redwolf/files_May/Asianwolves_gatk_chrX_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9.vcf
+/group/ctbrowngrp2/hennelly/hennelly/Pakistan_Graywolf_Genomics/Selection/Genes/IGF1region/shapeit/shapeit.v2.904.3.10.0-693.11.6.el7.x86_64/bin/shapeit -convert  --input-haps ${XCHR} --output-vcf ${OUTVCF}
