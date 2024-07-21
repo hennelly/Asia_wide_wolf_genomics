9f482d7b08aee433b0b16ea01a1a3ac2259f8acb
Lauren Hennelly <lmhennelly@ucdavis.edu>
2024-07-21 11:48:54 -0400

    Update 02_ASTRAL_Xchr_subset.sh

--- 05_Phylogenomics/02_Xchromosome/02_ASTRAL_Xchr_subset.sh
+++ 05_Phylogenomics/02_Xchromosome/02_ASTRAL_Xchr_subset.sh
@@ -3,14 +3,24 @@
 #SBATCH -c 1
 #SBATCH --time 01:10:00
 #SBATCH --mem-per-cpu 1G
-#SBATCH -o /home/crq857/projects/Chapter2/slurmout_Oct/astral_chrX_extract_%A_%a.out
-#SBATCH -e /home/crq857/projects/Chapter2/slurmout_Oct/astral_chrX_extract_%A_%a.err
+#SBATCH -o /home/crq857/projects/Chapter2/slurmout_Oct/astral_chrX_extract_nodogsadmix_%A_%a.out
+#SBATCH -e /home/crq857/projects/Chapter2/slurmout_Oct/astral_chrX_extract_nodogsadmix_%A_%a.err
 #SBATCH --array=1-500%10
 
 BEDFILE=$(sed "${SLURM_ARRAY_TASK_ID}q;d" /projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/bedfiles_chrX/listfiles.txt)
-DIR=/projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/input_chrX_March2024
-VCF=/projects/mjolnir1/people/crq857/Chapter2/06_Datasets/Xchr_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9_forASTRAl.recode.vcf
+DIR=/projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/input_chrX_July2024_all
+VCF=/projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/VCFs_July2024/chrX_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9_forASTRAl_nodogs.recode.vcf 
 
 module load bedtools
 
-bedtools intersect -a ${VCF} -b /projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/bedfiles_chrX/${BEDFILE} > ${DIR}/ChrX_allwolves_March2024_chrX_${BEDFILE}.vcf
+#All except dogs
+bedtools intersect -a ${VCF} -b /projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/bedfiles_chrX/${BEDFILE} > ${DIR}/ChrX_allwolves_March2024_chrX_${BEDFILE}_allexceptdogsJuly2024.vcf
+
+VCF_noadmix=/projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/VCFs_July2024/chrX_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9_forASTRAl_nodogs_noLadakhPakistanTajWestAsia.recode.vcf
+DIR_noadmix=/projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/input_chrX_July2024_noadmix
+
+#No admix and dogs 
+bedtools intersect -a ${VCF_noadmix} -b /projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/bedfiles_chrX/${BEDFILE} > ${DIR_noadmix}/ChrX_allwolves_March2024_chrX_${BEDFILE}_noadmixwolvesJuly2024.vcf
+
+
+
