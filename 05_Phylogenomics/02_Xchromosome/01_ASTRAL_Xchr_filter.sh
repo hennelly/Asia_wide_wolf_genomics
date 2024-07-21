5b5821eb3f559714163debb98830d448130578e7
Lauren Hennelly <lmhennelly@ucdavis.edu>
2024-07-21 11:19:14 -0400

    Update 01_ASTRAL_Xchr_filter.sh

--- 05_Phylogenomics/02_Xchromosome/01_ASTRAL_Xchr_filter.sh
+++ 05_Phylogenomics/02_Xchromosome/01_ASTRAL_Xchr_filter.sh
@@ -1,16 +1,25 @@
 #!/usr/bin/env bash
 #SBATCH --job-name=astral
 #SBATCH -c 1
-#SBATCH --time 11:10:00
+#SBATCH --time 1-11:10:00
 #SBATCH --mem-per-cpu 3G
-#SBATCH -o /home/crq857/projects/Chapter2/slurmout_Oct/astral_March2024_chrX.out
-#SBATCH -e /home/crq857/projects/Chapter2/slurmout_Oct/astral_March2024_chrX.err
+#SBATCH -o /home/crq857/projects/Chapter2/slurmout_Oct/astral_March2024_chrX_nodogsLadakj.out
+#SBATCH -e /home/crq857/projects/Chapter2/slurmout_Oct/astral_March2024_chrX_nodogsLadakj.err
 
 module load perl
 module load vcftools
 
-XCHR=/projects/mjolnir1/people/crq857/Chapter2/00_Alignment/05_GenotypeGATK/gatk_chrX_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9.recode.vcf
-REMOVE=remove.txt
-XOUT=/projects/mjolnir1/people/crq857/Chapter2/06_Datasets/Xchr_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9_forASTRAl
+#X chromosome - all samples except dogs
+VCF=/projects/mjolnir1/people/crq857/Chapter2/00_Alignment/05_GenotypeGATK/gatk_chrX_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9.recode.vcf.gz
+REMOVE=/home/crq857/projects/Chapter2/files/ASTRAL_removedogs.txt
+OUT=/projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/VCFs_July2024/chrX_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9_forASTRAl_nodogs
 
-vcftools --vcf ${XCHR} --remove ${REMOVE} --recode --recode-INFO-all --out ${XOUT}
+vcftools --gzvcf ${VCF} --remove ${REMOVE} --recode --recode-INFO-all --out ${OUT}
+
+#X chromosome -  no Pakistan, Ladakh, Tajikistan, West Asia, no dogs
+VCF=/projects/mjolnir1/people/crq857/Chapter2/00_Alignment/05_GenotypeGATK/gatk_chrX_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9.recode.vcf.gz
+REMOVE=/home/crq857/projects/Chapter2/files/ASTRAL_remove_admix.txt
+OUT=/projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/VCFs_July2024/chrX_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9_forASTRAl_nodogs_noLadakhPakistanTajWestAsia
+
+vcftools --gzvcf ${VCF} --remove ${REMOVE} --recode --recode-INFO-all --out ${OUT}
+
