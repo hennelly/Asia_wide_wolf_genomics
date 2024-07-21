a69a0324a07e5676b193868d87675225d5f3bd58
Lauren Hennelly <lmhennelly@ucdavis.edu>
2024-07-21 11:43:19 -0400

    Update 01_twisst_vcf2geno.sh

--- 03_Geneflow_Analyses/01_twisst/01_twisst_vcf2geno.sh
+++ 03_Geneflow_Analyses/01_twisst/01_twisst_vcf2geno.sh
@@ -27,3 +27,34 @@ python3 /projects/mjolnir1/people/crq857/bin/genomics_general/VCF_processing/par
 
 
 
+/home/crq857/projects/Chapter2/scripts_Dec/out_autosomes.nex
+
+scp -r crq857@mjolnirgate.unicph.domain:/home/crq857/projects/Chapter2/scripts_Dec/out_autosomes.nex   ~/Desktop
+
+#!/usr/bin/env bash
+#SBATCH --job-name=twisst
+#SBATCH --nodes 1
+#SBATCH --ntasks 1
+#SBATCH --time 4-12:00:00
+#SBATCH --mem=10GB
+#SBATCH -o /home/hennelly/Asianwolves/slurmout/02_twisst_Asianwolves_final_Tib_nnewtwisst_EastAsia_%A_%a.out
+#SBATCH -e /home/hennelly/Asianwolves/slurmout/02_twisst_Asianwolves_final_Tib_nnewtwisst_EastAsia_%A_%a.err
+#SBATCH -p high
+#SBATCH --array=2-38%15
+
+echo "My SLURM_ARRAY_TASK_ID: " $SLURM_ARRAY_TASK_ID
+CHR=$(sed "${SLURM_ARRAY_TASK_ID}q;d" /home/hennelly/Redwolf/files/chrlist.txt | cut -f1)
+
+DIR=/group/ctbrowngrp2/hennelly/hennelly/Asianwolves/phyml
+OUTDIR=/group/ctbrowngrp2/hennelly/hennelly/Asianwolves/output
+
+conda activate ete3_python3
+
+
+popfileTib=/group/ctbrowngrp2/hennelly/hennelly/Asianwolves/phyml/TwisstTibetan_popfile.txt
+#For Tajikistan wolves
+python3 /home/hennelly/Redwolf/bin/twisst/twisst.py -t ${DIR}/gatk_chr${CHR}_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9.final.phyml.trees.gz -w ${OUTDIR}/gatk_chr${CHR}_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9.final_twisst_EastAsia -g Tibetan -g EastAsia -g Russia -g Europe -g Andeanfox --outgroup Andeanfox --method complete --groupsFile ${popfileTib}
+
+
+
+
