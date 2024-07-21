ca0053cd95ab096dac8fecb7fb088f38a1ed13b8
Lauren Hennelly <lmhennelly@ucdavis.edu>
2024-07-21 15:59:54 -0400

    Update 02_twisst_geno2phy.sh

--- 03_Geneflow_Analyses/01_twisst/02_twisst_geno2phy.sh
+++ 03_Geneflow_Analyses/01_twisst/02_twisst_geno2phy.sh
@@ -3,17 +3,15 @@
 #SBATCH -c 1
 #SBATCH --time 1-10:30:00
 #SBATCH --mem-per-cpu 5G
-#SBATCH -o /home/crq857/projects/Chapter2/slurmout/twisst_phy_%A_%a.out
-#SBATCH -e /home/crq857/projects/Chapter2/slurmout/twisst_phy_%A_%a.err
-#SBATCH --array=1-38%5
+#SBATCH -o /home/crq857/projects/Chapter2/slurmout/twisst_phy_chrX.out
+#SBATCH -e /home/crq857/projects/Chapter2/slurmout/twisst_phy_chrX.err
 
-CHR=$(sed "${SLURM_ARRAY_TASK_ID}q;d" /home/crq857/projects/Geneflow_Dogs/files/chrlist_angsd.txt | cut -f1)
 
-DIR=/projects/mjolnir1/people/crq857/Chapter2/03_GeneflowAnalyses/twisst/geno/
+PHY=/projects/mjolnir1/people/crq857/Chapter2/06_Datasets/Asianwolves_gatk_chrX_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9.geno.gz
 OUTDIR=/projects/mjolnir1/people/crq857/Chapter2/03_GeneflowAnalyses/twisst/phyml
 
 module load python/2.7.18
 module load numpy
 module load phyml
 
-python /projects/mjolnir1/people/crq857/bin/genomics_general/phylo/phyml_sliding_windows.py -T 1 -g ${DIR}/gatk_${CHR}_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9.final.geno.gz --outgroup AndeanFox -w 100 --windType sites --prefix ${OUTDIR}/gatk_${CHR}_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9.final.phyml --model GTR
+python /projects/mjolnir1/people/crq857/bin/genomics_general/phylo/phyml_sliding_windows.py -T 1 -g ${PHY} --outgroup AndeanFox -w 100 --windType sites --prefix ${OUTDIR}/gatk_chrX_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9.final.phyml --model GTR
