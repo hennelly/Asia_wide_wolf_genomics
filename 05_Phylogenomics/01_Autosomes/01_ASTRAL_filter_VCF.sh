85d76ac09190b355ce1597abc0d51489b6f854fa
Lauren Hennelly <lmhennelly@ucdavis.edu>
2024-07-24 12:49:24 -0400

    Update 01_ASTRAL_filter_VCF.sh

--- 05_Phylogenomics/01_Autosomes/01_ASTRAL_filter_VCF.sh
+++ 05_Phylogenomics/01_Autosomes/01_ASTRAL_filter_VCF.sh
@@ -3,8 +3,8 @@
 #SBATCH -c 1
 #SBATCH --time 1-11:10:00
 #SBATCH --mem-per-cpu 3G
-#SBATCH -o /home/crq857/projects/Chapter2/slurmout_Oct/astral_March2024_chrX_nodogsLadakj.out
-#SBATCH -e /home/crq857/projects/Chapter2/slurmout_Oct/astral_March2024_chrX_nodogsLadakj.err
+#SBATCH -o /home/crq857/projects/Chapter2/slurmout_Oct/astral_March2024_chrX_nodogsLadakj_withNA.out
+#SBATCH -e /home/crq857/projects/Chapter2/slurmout_Oct/astral_March2024_chrX_nodogsLadakj_withNA.err
 
 module load perl
 module load vcftools
@@ -12,7 +12,7 @@ module load vcftools
 #Autosomes - all samples except dogs
 VCF=/projects/mjolnir1/people/crq857/Chapter2/00_Alignment/05_GenotypeGATK/Autosomes_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9.recode.vcf.vcf.gz
 REMOVE=/home/crq857/projects/Chapter2/files/ASTRAL_removedogs.txt
-OUT=/projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/VCFs_July2024/Autosomes_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9_forASTRAl_nodogs
+OUT=/projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/VCFs_July2024/Autosomes_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9_forASTRAl_nodogs_withNAwolves
 
 vcftools --gzvcf ${VCF} --remove ${REMOVE} --recode --recode-INFO-all --out ${OUT}
 
