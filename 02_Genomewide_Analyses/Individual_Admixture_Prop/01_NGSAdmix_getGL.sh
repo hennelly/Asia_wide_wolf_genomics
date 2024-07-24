1319a664e91ef79b11da32fbf905a9608d4f30d3
Lauren Hennelly <lmhennelly@ucdavis.edu>
2024-07-24 14:04:31 -0400

    Create 01_NGSAdmix_getGL.sh

---
+++ 02_Genomewide_Analyses/Individual_Admixture_Prop/01_NGSAdmix_getGL.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+#SBATCH --job-name=angsd
+#SBATCH -c 1
+#SBATCH --time 5-12:00:00
+#SBATCH --mem-per-cpu 20G
+#SBATCH -o /home/crq857/projects/Chapter2/slurmout_Oct/angsd_Dec21_NGSadmix_%A_%a.out
+#SBATCH -e /home/crq857/projects/Chapter2/slurmout_Oct/angsd_Dec21_NGSadmix_%A_%a.err
+#SBATCH --array=1-38%5
+
+# Eurasian wolves and basal canids + dogs  
+
+CHR=$(sed -n "$SLURM_ARRAY_TASK_ID"p /home/crq857/projects/Geneflow_Dogs/files/chrlist_angsd.txt | awk '{print $1}')
+REF=/projects/mjolnir1/people/crq857/Chapter2/ref/canFam31.fasta
+LIST=/home/crq857/projects/Chapter2/list_Sept/bamlist_for_ngsAdmix.txt
+OUTDIR=/projects/mjolnir1/people/crq857/Chapter2/02_GenomewideAnalyses/Beaglefiles
+
+module load angsd
+
+angsd -out ${OUTDIR}/Allcanids_Dec26_NGSAdmix_${CHR} -bam ${LIST} -ref ${REF} -nThreads 10 \
+    -GL 1 \
+    -checkBamHeaders 0 \
+    -trim 0 -C 50 -baq 1 \
+    -minMapQ 20 -minQ 20 -minInd 102 \
+    -uniqueOnly 1 -remove_bads 1 \
+    -noTrans 1 -doGlf 2 -doMajorMinor 1 -doMaf 1 \
+    -SNP_pval 1e-6 \
+    -P 10 \
+    -r ${CHR}:
