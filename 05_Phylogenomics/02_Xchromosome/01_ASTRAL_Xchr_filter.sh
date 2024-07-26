d2b03683985c6eb3b0e8da41db4dfd58187e51da
Lauren Hennelly <lmhennelly@ucdavis.edu>
2024-07-26 14:45:47 -0400

    Update 01_ASTRAL_Xchr_filter.sh

--- 05_Phylogenomics/02_Xchromosome/01_ASTRAL_Xchr_filter.sh
+++ 05_Phylogenomics/02_Xchromosome/01_ASTRAL_Xchr_filter.sh
@@ -3,8 +3,8 @@
 #SBATCH -c 1
 #SBATCH --time 1-11:10:00
 #SBATCH --mem-per-cpu 3G
-#SBATCH -o /home/crq857/projects/Chapter2/slurmout_Oct/astral_March2024_chrX_nodogsLadakj.out
-#SBATCH -e /home/crq857/projects/Chapter2/slurmout_Oct/astral_March2024_chrX_nodogsLadakj.err
+#SBATCH -o /home/crq857/projects/Chapter2/slurmout_Oct/astral_xchr_noadmix.out
+#SBATCH -e /home/crq857/projects/Chapter2/slurmout_Oct/astral_xchr_noadmix.err
 
 module load perl
 module load vcftools
@@ -12,15 +12,16 @@ module load vcftools
 #X chromosome - all samples except dogs
 VCF=/projects/mjolnir1/people/crq857/Chapter2/00_Alignment/05_GenotypeGATK/gatk_chrX_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9.recode.vcf.gz
 REMOVE=/home/crq857/projects/Chapter2/files/July26_taxaremove_onlydogs_badwolves.txt
-OUT=/projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/VCFs_July2024/chrX_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9_forASTRAl_nodogs
+OUT=/projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/xFINAL_VCFs_July262024/July26chrX_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9_forASTRAl_nodogs
 
 vcftools --gzvcf ${VCF} --remove ${REMOVE} --recode --recode-INFO-all --out ${OUT}
 
-#X chromosome -  no Pakistan, Ladakh, Tajikistan, West Asia, no dogs
+#X chromosome -  no Pakistan, Ladakh, Tajikistan, West Asia, no dogs, no NA wolves
 VCF=/projects/mjolnir1/people/crq857/Chapter2/00_Alignment/05_GenotypeGATK/gatk_chrX_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9.recode.vcf.gz
-REMOVE=/home/crq857/projects/Chapter2/files/ASTRAL_remove_admix.txt
-OUT=/projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/VCFs_July2024/chrX_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9_forASTRAl_nodogs_noLadakhPakistanTajWestAsia
+REMOVE=/home/crq857/projects/Chapter2/files/July26_taxaremove_onlydogs_admixwolves.txt
+OUT=/projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/xFINAL_VCFs_July262024/July26chrX_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9_forASTRAl_nodogs_noLadakhPakistanTajWestAsia
 
 vcftools --gzvcf ${VCF} --remove ${REMOVE} --recode --recode-INFO-all --out ${OUT}
 
-July26_taxaremove_onlydogs_badwolves.txt
+REMOVE=/home/crq857/projects/Chapter2/files/July26_taxaremove_onlydogs_badwolves.txt
+REMOVE=/home/crq857/projects/Chapter2/files/July26_taxaremove_onlydogs_admixwolves.txt
