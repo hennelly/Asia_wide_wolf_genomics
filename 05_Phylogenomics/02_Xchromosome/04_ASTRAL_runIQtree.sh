81bd7ed9d94993c820dc8cb6493dfaa3d9121dc8
Lauren Hennelly <lmhennelly@ucdavis.edu>
2024-07-26 15:46:17 -0400

    Update 04_ASTRAL_runIQtree.sh

--- 05_Phylogenomics/02_Xchromosome/04_ASTRAL_runIQtree.sh
+++ 05_Phylogenomics/02_Xchromosome/04_ASTRAL_runIQtree.sh
@@ -8,20 +8,20 @@
 #SBATCH --array=1-500%10
 
 #conda activate /projects/mjolnir1/apps/conda/python-3.5.6
-#cd /projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/phyfiles_chrX_July2024_all
-#ls  *phy > listfileschrX.txt_phy.txt
+#cd /projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/xFINAL_chrX_July262026_phyfiles_nodogs_withNA
+#ls  *phy > FINAL_listfileschrX.txt_phy.txt
 
-PHY=$(sed "${SLURM_ARRAY_TASK_ID}q;d" /projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/phyfiles_chrX_July2024_all/filelist_chrX_all_phy.txt)
+PHY=$(sed "${SLURM_ARRAY_TASK_ID}q;d" /projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/xFINAL_chrX_July262026_phyfiles_nodogs_withNA/FINAL_listfileschrX.txt_phy.txt)
 
-/home/crq857/bin/iqtree-1.6.12-Linux/bin/iqtree -s  /projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/phyfiles_chrX_July2024_all/${PHY} -bb 1000 -nt AUTO
+/home/crq857/bin/iqtree-1.6.12-Linux/bin/iqtree -s  /projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/xFINAL_July26_phyfiles_chrX_nodogswithNA/${PHY} -bb 1000 -nt AUTO
 
 ### NO ADMIXED
 
 #cd /projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/phyfiles_chrX_July2024_noadmix
-#ls  *phy > listfileschrX_noadmix.txt_phy.txt
+#ls  *phy > FINAL_listfileschrX_noadmix.txt_phy.txt
 
-PHY=$(sed "${SLURM_ARRAY_TASK_ID}q;d" /projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/phyfiles_chrX_July2024_noadmix/listfileschrX_noadmix.txt_phy.txt)
+PHY=$(sed "${SLURM_ARRAY_TASK_ID}q;d" /projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/phyfiles_chrX_July2024_noadmix/FINAL_listfileschrX_noadmix.txt_phy.txt)
 
-/home/crq857/bin/iqtree-1.6.12-Linux/bin/iqtree -s  /projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/phyfiles_chrX_July2024_noadmix/${PHY} -bb 1000 -nt AUTO
+/home/crq857/bin/iqtree-1.6.12-Linux/bin/iqtree -s  /projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/xFINAL_July26_phyfiles_chrX_July2024_noadmix/${PHY} -bb 1000 -nt AUTO
 
 
