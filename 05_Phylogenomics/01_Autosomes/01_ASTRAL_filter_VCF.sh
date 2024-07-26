4e88ab7fb670fb817fc49fe6af83b1311befc74e
Lauren Hennelly <lmhennelly@ucdavis.edu>
2024-07-26 14:49:13 -0400

    Update 01_ASTRAL_filter_VCF.sh

--- 05_Phylogenomics/01_Autosomes/01_ASTRAL_filter_VCF.sh
+++ 05_Phylogenomics/01_Autosomes/01_ASTRAL_filter_VCF.sh
@@ -3,24 +3,25 @@
 #SBATCH -c 1
 #SBATCH --time 1-11:10:00
 #SBATCH --mem-per-cpu 3G
-#SBATCH -o /home/crq857/projects/Chapter2/slurmout_Oct/astral_March2024_chrX_nodogsLadakj_withNA.out
-#SBATCH -e /home/crq857/projects/Chapter2/slurmout_Oct/astral_March2024_chrX_nodogsLadakj_withNA.err
+#SBATCH -o /home/crq857/projects/Chapter2/slurmout_Oct/astral_March2024_chrX_nodogsLadakj_withNAnoadmix.out
+#SBATCH -e /home/crq857/projects/Chapter2/slurmout_Oct/astral_March2024_chrX_nodogsLadakj_withNAnoadmix.err
 
 module load perl
 module load vcftools
 
 #Autosomes - all samples except dogs
 VCF=/projects/mjolnir1/people/crq857/Chapter2/00_Alignment/05_GenotypeGATK/Autosomes_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9.recode.vcf.vcf.gz
-REMOVE=/home/crq857/projects/Chapter2/files/ASTRAL_removedogs.txt
-OUT=/projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/VCFs_July2024/Autosomes_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9_forASTRAl_nodogs_withNAwolves
+REMOVE=/home/crq857/projects/Chapter2/files/July26_taxaremove_onlydogs_badwolves.txt
+OUT=/projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/xFINAL_VCFs_July262024/Autosomes_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9_forASTRAl_nodogs_withNAwolves
 
 vcftools --gzvcf ${VCF} --remove ${REMOVE} --recode --recode-INFO-all --out ${OUT}
 
 #Autosomes - no Pakistan, Ladakh, Tajikistan, West Asia, no dogs
-VCF=/projects/mjolnir1/people/crq857/Chapter2/00_Alignment/05_GenotypeGATK/Autosomes_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9.recode.vcf.vcf.gz
-REMOVE=/home/crq857/projects/Chapter2/files/ASTRAL_remove_admix.txt
-OUT=/projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/VCFs_July2024/Autosomes_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9_forASTRAl_nodogs_noLadakhPakistanTajWestAsia
+VCFnoadmix=/projects/mjolnir1/people/crq857/Chapter2/00_Alignment/05_GenotypeGATK/Autosomes_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9.recode.vcf.vcf.gz
+REMOVEnoadmix=/home/crq857/projects/Chapter2/files/July26_taxaremove_onlydogs_admixwolves.txt
+OUTnoadmix=/projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/xFINAL_VCFs_July262024/Autosomes_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9_forASTRAl_nodogs_noNAwolvesLadakhPakistanTajWestAsia
 
+vcftools --gzvcf ${VCFnoadmix} --remove ${REMOVEnoadmix} --recode --recode-INFO-all --out ${OUTnoadmix}
 
 
 
