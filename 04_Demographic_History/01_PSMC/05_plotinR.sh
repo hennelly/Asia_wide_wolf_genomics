51bb38112560019823b8e425ea8bb46d41315438
Lauren Hennelly <lmhennelly@ucdavis.edu>
2024-07-26 16:45:57 -0400

    Update 05_plotinR.sh

--- 04_Demographic_History/01_PSMC/05_plotinR.sh
+++ 04_Demographic_History/01_PSMC/05_plotinR.sh
@@ -1,3 +1,11 @@
 
 scp -r crq857@mjolnirgate.unicph.domain:/projects/mjolnir1/people/crq857/Chapter2/04_Demographichistory/PSMC/plot_recheck/*.txt   ~/Desktop
 /projects/mjolnir1/people/crq857/Chapter2/04_Demographichistory/PSMC/plot_recheck/*.txt
+
+
+
+scp -r crq857@mjolnirgate.unicph.domain:/projects/mjolnir1/people/crq857/Chapter2/06_Datasets/Asianwolves_gatk_chrX_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9.header   ~/Desktop
+/projects/mjolnir1/people/crq857/Chapter2/04_Demographichistory/PSMC/plot_recheck/*.txt
+
+/projects/mjolnir1/people/crq857/Chapter2/06_Datasets/Asianwolves_gatk_chrX_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9.header
+
