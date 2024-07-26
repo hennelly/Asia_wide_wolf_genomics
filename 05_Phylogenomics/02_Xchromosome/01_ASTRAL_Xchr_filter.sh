48df1cc3ff9e0248e926912f8e317303b35f49de
Lauren Hennelly <lmhennelly@ucdavis.edu>
2024-07-26 14:40:36 -0400

    Update 01_ASTRAL_Xchr_filter.sh

--- 05_Phylogenomics/02_Xchromosome/01_ASTRAL_Xchr_filter.sh
+++ 05_Phylogenomics/02_Xchromosome/01_ASTRAL_Xchr_filter.sh
@@ -11,7 +11,7 @@ module load vcftools
 
 #X chromosome - all samples except dogs
 VCF=/projects/mjolnir1/people/crq857/Chapter2/00_Alignment/05_GenotypeGATK/gatk_chrX_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9.recode.vcf.gz
-REMOVE=/home/crq857/projects/Chapter2/files/ASTRAL_removedogs.txt
+REMOVE=/home/crq857/projects/Chapter2/files/July26_taxaremove_onlydogs_badwolves.txt
 OUT=/projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/VCFs_July2024/chrX_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9_forASTRAl_nodogs
 
 vcftools --gzvcf ${VCF} --remove ${REMOVE} --recode --recode-INFO-all --out ${OUT}
@@ -23,3 +23,4 @@ OUT=/projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/VCFs_July2024/chr
 
 vcftools --gzvcf ${VCF} --remove ${REMOVE} --recode --recode-INFO-all --out ${OUT}
 
+July26_taxaremove_onlydogs_badwolves.txt
