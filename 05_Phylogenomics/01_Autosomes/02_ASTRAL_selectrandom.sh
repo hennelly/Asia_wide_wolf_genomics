524e0091731d826223777cd7f227d0d73d23c365
Lauren Hennelly <lmhennelly@ucdavis.edu>
2024-07-28 11:46:49 -0400

    Update 02_ASTRAL_selectrandom.sh

--- 05_Phylogenomics/01_Autosomes/02_ASTRAL_selectrandom.sh
+++ 05_Phylogenomics/01_Autosomes/02_ASTRAL_selectrandom.sh
@@ -3,7 +3,7 @@
 #SBATCH -c 1
 #SBATCH --time 1:10:00
 #SBATCH --mem-per-cpu 1G
-#SBATCH -o /home/crq857/projects/Geneflow_Dogs/slurmout/bedtoolsrandom.out
+#SBATCH -o /home/crq857/projects/Geneflow_Dogs/slurmout/bedtoolsrandom_again2.out
 #SBATCH -e /home/crq857/projects/Geneflow_Dogs/slurmout/bedtoolsrandom.err
 
 GENOME=/projects/mjolnir1/people/crq857/Chapter2/ref/canFam31.fasta.fai
@@ -11,3 +11,20 @@ GENOME=/projects/mjolnir1/people/crq857/Chapter2/ref/canFam31.fasta.fai
 module load bedtools 
 
 bedtools random -g ${GENOME} -l 20000 -n 5000 
+
+#Grab X chromosome regions
+grep "chrX" /home/crq857/projects/Geneflow_Dogs/slurmout/bedtoolsrandom_again.out >  /home/crq857/projects/Geneflow_Dogs/slurmout/bedtoolsrandom_again_chrX.out #44 regions
+grep "chrX" /home/crq857/projects/Geneflow_Dogs/slurmout/bedtoolsrandom_again2.out >  /home/crq857/projects/Geneflow_Dogs/slurmout/bedtoolsrandom_again_chrX2.out #42 regions
+cat   /home/crq857/projects/Geneflow_Dogs/slurmout/bedtoolsrandom_again_chrX.out  /home/crq857/projects/Geneflow_Dogs/slurmout/bedtoolsrandom_again_chrX2.out > more_randomregions_chrX.txt
+
+#clean up output - take only first three columns
+
+awk '{print $1,$2,$3}' more_randomregions_chrX.txt > more_randomregions_chrX.bed
+tr ' ' \\t < more_randomregions_chrX.bed > more_randomregions_chrX_tabbed.bed
+
+#write each line of bed file as its own file
+
+FILE=/home/crq857/projects/Chapter2/scripts_PhylogeneticsJuly/more_randomregions_chrX_tabbed.bed
+
+awk '!/^$/{print  > "File_"NR".txt" }' ${FILE}
+
