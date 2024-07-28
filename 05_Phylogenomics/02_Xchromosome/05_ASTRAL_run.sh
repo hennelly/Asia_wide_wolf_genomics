efe1a3920d9a706b519675aa9c54e7a08b05b920
Lauren Hennelly <lmhennelly@ucdavis.edu>
2024-07-28 11:09:37 -0400

    Update 05_ASTRAL_run.sh

--- 05_Phylogenomics/02_Xchromosome/05_ASTRAL_run.sh
+++ 05_Phylogenomics/02_Xchromosome/05_ASTRAL_run.sh
@@ -8,15 +8,23 @@
 
 #make a list of the treefiles 
 #cd /projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/phyfiles_chrX_July2024_all
-# cat *treefile > listtreefiles.txt
+# cat *treefile > listtreefiles_nodogswithNA.txt
 
 conda activate /projects/mjolnir1/apps/conda/astral-2.2
 module load jdk
 
-TREE=/projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/phyfiles_chrX_July2024_all/listtreefiles.txt
-OUTDIR=/projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/chrX_ASTRAL_results_July2024
+## NO DOGS WITH NA 
+TREE=/projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/xFINAL_chrX_July262026_phyfiles_nodogs_withNA/listtreefiles_nodogswithNA.txt
+OUTDIR=/projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/xFINAL_chrX_July262026_phyfiles_nodogs_withNA_results
 
-java -jar /projects/mjolnir1/people/crq857/Chapter2/bin/ASTER/ASTRAL/astral.5.7.8.jar -i ${TREE} -o ${OUTDIR}/chrX_July2024_allsamples_exceptdogs.tre
+java -jar /projects/mjolnir1/people/crq857/Chapter2/bin/ASTER/ASTRAL/astral.5.7.8.jar -i ${TREE} -o ${OUTDIR}/July28_2024_chrX_nodogs_withNA.tre
 
 
-/projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/phyfiles_chrX_July2024_all/ChrX_allwolves_March2024_chrX_File_104.txt_allexceptdogsJuly2024.vcf_phy.min4.phy.treefile
+
+## NO ADMIX
+TREE=/projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/xFINAL_chrX_July262026_phyfiles_noadmix/listtreefiles_Xchr_noadmix.txt
+OUTDIR=/projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/xFINAL_chrX_July262026_phyfiles_noadmix_results
+
+java -jar /projects/mjolnir1/people/crq857/Chapter2/bin/ASTER/ASTRAL/astral.5.7.8.jar -i ${TREE} -o ${OUTDIR}/July28_2024_chrX_noadmix.tre
+
+
