38a1677862fa8fd6b09d69799f80924fcaff33c6
Lauren Hennelly <lmhennelly@ucdavis.edu>
2024-07-31 13:31:31 -0400

    Update 03_psmcfa2psmc.sh

--- 04_Demographic_History/01_PSMC/03_psmcfa2psmc.sh
+++ 04_Demographic_History/01_PSMC/03_psmcfa2psmc.sh
@@ -22,28 +22,5 @@ module load psmc
 psmc -p "1+1+1+1+25*2+4+6" -r5 -t15 -N25 -o ${OUTDIR}/${PSMCFA}_1_1_1_1.psmc ${DIR}/${PSMCFA}
 
 
-scp -r crq857@mjolnirgate.unicph.domain:/home/crq857/projects/Chapter2/scripts_June2024/*pdf   ~/Desktop
-/home/crq857/projects/Chapter2/scripts_June2024/*pdf
-
-#!/usr/bin/env bash
-#SBATCH --job-name=lworec
-#SBATCH -c 1
-#SBATCH --time 2-11:40:00
-#SBATCH --mem-per-cpu 50G
-#SBATCH -o /home/crq857/projects/Chapter2/slurmout/iqtree_Xchr_noadmix_try2.out
-#SBATCH -e /home/crq857/projects/Chapter2/slurmout/iqtree_Xchr_noadmix_try2.err
-
-#conda activate /projects/mjolnir1/apps/conda/python-3.5.6
-
-PHYnoad=/projects/mjolnir1/people/crq857/Chapter2/06_Datasets/Xchr_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9_forASTRAl_lowrecombination_noadmixPak.min4_copy.phy
-#/home/crq857/bin/iqtree-1.6.12-Linux/bin/iqtree -s ${PHY} -bb 1000 -nt AUTO -m MFP
-/home/crq857/bin/iqtree-1.6.12-Linux/bin/iqtree -s ${PHYnoad} -bb 1000 -nt AUTO -m MFP
-
-# you may need to download iqtree: http://www.iqtree.org/
-# the -m flag is to decide which substitution model best fits the data. The MFP flag is telling iqtree to estimate which model is best
-# the -bb flag is for bootstrapping. The 1000 tells iqtree to do 1000 bootstrap replicates
-
-
-
 
 
