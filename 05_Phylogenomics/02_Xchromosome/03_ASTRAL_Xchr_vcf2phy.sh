b6c75a5c10315fa3c8b914ba6cde9c0181eef3b2
Lauren Hennelly <lmhennelly@ucdavis.edu>
2024-07-31 21:37:38 -0400

    Update 03_ASTRAL_Xchr_vcf2phy.sh

--- 05_Phylogenomics/02_Xchromosome/03_ASTRAL_Xchr_vcf2phy.sh
+++ 05_Phylogenomics/02_Xchromosome/03_ASTRAL_Xchr_vcf2phy.sh
@@ -9,19 +9,19 @@
 
 #conda activate /projects/mjolnir1/apps/conda/python-3.5.6
 
-#cd /projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/xFINAL_chrX_July262026_input_July2024_nodogs_withNA
-# ls > FINAL_filelist_July2024_all_noNA.txt
+#cd /projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/xFINAL_chrX_July262026_input_July2024_nodogs_withNA_noEth
+# ls > FINAL_filelist_July2024_all_noNA_noEth.txt
 
-FILE=$(sed "${SLURM_ARRAY_TASK_ID}q;d" /projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/xFINAL_chrX_July262026_input_July2024_nodogs_withNA/FINAL_filelist_July2024_all_noNA.txt)
+FILE=$(sed "${SLURM_ARRAY_TASK_ID}q;d" /projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/xFINAL_chrX_July262026_input_July2024_nodogs_withNA_noEth/FINAL_filelist_July2024_all_noNA_noEth.txt)
 
 # ALL except dogs 
-DIR=/projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/xFINAL_chrX_July262026_input_July2024_nodogs_withNA
-OUTDIR=/projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/xFINAL_chrX_July262026_phyfiles_nodogs_withNA
-VCF=/projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/xFINAL_VCFs_July262024/July26chrX_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9_forASTRAl_nodogs.recode.vcf 
+DIR=/projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/xFINAL_chrX_July262026_input_July2024_nodogs_withNA_noEth
+OUTDIR=/projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/xFINAL_chrX_July262026_phy_July2024_nodogs_withNA_noEth
+VCF=/projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/xFINAL_VCFs_July262024/July30_NO_ETHIOPIAN_chrX_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9_forASTRAl_nodogs.recode.vcf  
 
 #grab and add header 
-grep "#" ${VCF} > /home/crq857/projects/Chapter2/files/FINAL_filelist_July2024_all_noNA.txt
-cat /home/crq857/projects/Chapter2/files/FINAL_filelist_July2024_all_noNA.txt ${DIR}/${FILE} > ${DIR}/${FILE}_header.vcf
+grep "#" ${VCF} > /home/crq857/projects/Chapter2/files/FINAL_filelist_July2024_all_noEth.txt
+cat /home/crq857/projects/Chapter2/files/FINAL_filelist_July2024_all_noEth.txt ${DIR}/${FILE} > ${DIR}/${FILE}_header.vcf
 
 #run vcf2phy
 python /projects/mjolnir1/people/crq857/Geneflow_Dogs/bin/vcf2phylip/vcf2phylip.py -i ${DIR}/${FILE}_header.vcf --output-folder ${OUTDIR} --output-prefix ${FILE}_phy -o Dhole_BerlinZoo
@@ -31,18 +31,18 @@ python /projects/mjolnir1/people/crq857/Geneflow_Dogs/bin/vcf2phylip/vcf2phylip.
 # No admix and dogs 
 #########################
 #########################
-#cd /projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/xFINAL_chrX_July262026_input_July2024_noadmix
-# ls > FINAL_filelist_July2024_noadmix.txt
+#cd /projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/xFINAL_chrX_July262026_input_July2024_noadmix_noEth
+# ls > FINAL_filelist_July2024_noadmix_noEth.txt
 
-FILE=$(sed "${SLURM_ARRAY_TASK_ID}q;d" /projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/xFINAL_chrX_July262026_input_July2024_noadmix/FINAL_filelist_July2024_noadmix.txt)
+FILE=$(sed "${SLURM_ARRAY_TASK_ID}q;d" /projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/xFINAL_chrX_July262026_input_July2024_noadmix_noEth/FINAL_filelist_July2024_noadmix_noEth.txt)
 
 # ALL except dogs 
-DIR=/projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/xFINAL_chrX_July262026_input_July2024_noadmix
-OUTDIR=/projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/xFINAL_chrX_July262026_phyfiles_noadmix
-VCF_noadmix=/projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/xFINAL_VCFs_July262024/July26chrX_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9_forASTRAl_nodogs_noLadakhPakistanTajWestAsia.recode.vcf
+DIR=/projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/xFINAL_chrX_July262026_input_July2024_noadmix_noEth
+OUTDIR=/projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/xFINAL_chrX_July262026_phy_July2024_noadmix_noEth
+VCF_noadmix=/projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/xFINAL_VCFs_July262024/July30chrX_NO_ETHIOPIAN_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9_forASTRAl_nodogs_noLadakhPakistanTajWestAsia.recode.vcf
 
 #grab and add header 
-grep "#" ${VCFnoadmix} > /home/crq857/projects/Chapter2/files/header_chrXJuly2024_noadmix.txt
+grep "#" /projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/xFINAL_VCFs_July262024/July30chrX_NO_ETHIOPIAN_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9_forASTRAl_nodogs_noLadakhPakistanTajWestAsia.recode.vcf > /home/crq857/projects/Chapter2/files/header_chrXJuly2024_noadmix_noEth.txt
 cat /home/crq857/projects/Chapter2/files/header_chrXJuly2024_noadmix.txt ${DIR}/${FILE} > ${DIR}/${FILE}_header.vcf
 #rm ${DIR}/${FILE}
 
