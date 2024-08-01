96fc9ca22081162b877b21f55b664f271bdf62d2
Lauren Hennelly <lmhennelly@ucdavis.edu>
2024-08-01 17:13:56 -0400

    Update 05_ASTRAL_runIQtree.sh

--- 05_Phylogenomics/01_Autosomes/05_ASTRAL_runIQtree.sh
+++ 05_Phylogenomics/01_Autosomes/05_ASTRAL_runIQtree.sh
@@ -5,16 +5,16 @@
 #SBATCH --mem-per-cpu 1G
 #SBATCH -o /home/crq857/projects/Geneflow_Dogs/slurmout/iqtree_fullgenome_run_%A_%a.out
 #SBATCH -e /home/crq857/projects/Geneflow_Dogs/slurmout/iqtree_fullgenome_run_%A_%a.err
-#SBATCH --array=1-1000%10
+#SBATCH --array=1-20%15
 
 #conda activate /projects/mjolnir1/apps/conda/python-3.5.6
 #cd /projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/x_FINAL_July262024_phy_autosomes_noadmix_noEth
-#ls  *phy > FINAL_list_autosomes_noadmix.txt_phy_noEth.txt
+#ls  *phy > FINAL_list_autosomes_noadmix.txt_phy_noEthmore.txt
 
 ### no dogs, with NA
-PHY=$(sed "${SLURM_ARRAY_TASK_ID}q;d" /projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/x_FINAL_July262024_phy_autosomes_nodogswithNA_noEth/FINAL_list_autosomes_nodogwithNA.txt_phy_noEth.txt)
+PHY=$(sed "${SLURM_ARRAY_TASK_ID}q;d" /projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/x_FINAL_July262024_input_autosomes_nodogswithNA_noEth_more/FINAL_list_autosomes_noadmix.txt_phy_noEthmore.txt)
 
-/home/crq857/bin/iqtree-1.6.12-Linux/bin/iqtree -s  /projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/x_FINAL_July262024_phy_autosomes_nodogswithNA_noEth/${PHY} -bb 1000 -nt AUTO
+/home/crq857/bin/iqtree-1.6.12-Linux/bin/iqtree -s  /projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/x_FINAL_July262024_input_autosomes_nodogswithNA_noEth_more/${PHY} -bb 1000 -nt AUTO
 
 ## no admix
 
