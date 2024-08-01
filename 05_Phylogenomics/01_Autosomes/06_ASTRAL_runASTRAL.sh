266a9a821f72abe1206e3421558d65d115f48e50
Lauren Hennelly <lmhennelly@ucdavis.edu>
2024-08-01 18:04:56 -0400

    Update 06_ASTRAL_runASTRAL.sh

--- 05_Phylogenomics/01_Autosomes/06_ASTRAL_runASTRAL.sh
+++ 05_Phylogenomics/01_Autosomes/06_ASTRAL_runASTRAL.sh
@@ -7,13 +7,16 @@
 #SBATCH -e /home/crq857/projects/Geneflow_Dogs/slurmout/astral_auto_byspecies_atry2ll_feb9_annotate.err
 
 #991 trees
-cd /projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/x_FINAL_July262024_phy_autosomes_nodogswithNA_noEth
-cat *treefile > listtreefiles_nodogswithNA_more_noEth_autosomes.txt
+#cd /projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/x_FINAL_July262024_phy_autosomes_nodogswithNA_noEth
+#cat *treefile > listtreefiles_nodogswithNA_more_noEth_autosomes.txt
 
+#cd /projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/x_FINAL_July262024_input_autosomes_nodogswithNA_noEth_more
+#cat *treefile > listtreefiles_nodogswithNA_more_noEth_autosomes_more.txt
+#cat listtreefiles_nodogswithNA_more_noEth_autosomes_more.txt /projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/x_FINAL_July262024_phy_autosomes_nodogswithNA_noEth/listtreefiles_nodogswithNA_more_noEth_autosomes.txt > listtreefiles_nodogswithNA_more_noEth_autosomes_all.txt 
 
-TREE=alltreefiles_autosomes.treefile
-
-java -jar /projects/mjolnir1/people/crq857/Chapter2/bin/ASTER/ASTRAL/astral.5.7.8.jar --branch-annotate 1 -i ${TREE} -o full_Feb9_try2_autosomes_branchannotate.tre
+TREE=/projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/x_FINAL_July262024_input_autosomes_nodogswithNA_noEth_more/listtreefiles_nodogswithNA_more_noEth_autosomes_all_1000.txt
+OUTDIR=/projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/x_FINAL_July262024_input_autosomes_nodogswithNA_noEth_more_results
+java -jar /projects/mjolnir1/people/crq857/Chapter2/bin/ASTER/ASTRAL/astral.5.7.8.jar --branch-annotate 1 -i ${TREE} -o ${OUTDIR}/full_Aug1_autosomes_branchannotate.tre
 
 
 
