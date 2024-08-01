aee1df26c2a1b6410f29522e5c7be99754affbca
Lauren Hennelly <lmhennelly@ucdavis.edu>
2024-08-01 18:09:42 -0400

    Update 03_psmcfa2psmc.sh

--- 04_Demographic_History/01_PSMC/03_psmcfa2psmc.sh
+++ 04_Demographic_History/01_PSMC/03_psmcfa2psmc.sh
@@ -7,22 +7,18 @@
 #SBATCH -o /home/crq857/projects/Chapter2/slurmout/PSMC_psmc_rerun_testlc_%A_%a.out
 #SBATCH -e /home/crq857/projects/Chapter2/slurmout/PSMC_psmc_rerun_testlc_%A_%a.err
 
-echo "My SLURM_ARRAY_TASK_ID: " $SLURM_ARRAY_TASK_ID
-PSMCFA=$(sed "${SLURM_ARRAY_TASK_ID}q;d" /home/crq857/projects/Chapter2/list_Dec/psmcfa_highcov.txt | cut -f1)
 DIR=/projects/mjolnir1/people/crq857/Chapter2/04_Demographichistory/PSMC/psmcfa
-OUTDIR=/projects/mjolnir1/people/crq857/Chapter2/04_Demographichistory/PSMC/psmc
+LOW=Chinese_CAN6.CanFam31_12x.psmcfa
+HIGH=Chinese_CAN6.CanFam31.realigned.bam_autosomes.bam.psmcfa
+OUTDIR=/projects/mjolnir1/people/crq857/Chapter2/04_Demographichistory/PSMC/plot_Shanxi
 
 module load psmc
 
-#psmc -p "4+25*2+4+6" -r5 -t15 -N25 -o ${OUTDIR}/${PSMCFA}_4.psmc ${DIR}/${PSMCFA}
-
-## Also try other combinations of input to see if it impacts the PSMC curve 
-
-#psmc -p "2+2+25*2+4+6" -r5 -t15 -N25 -o ${OUTDIR}/${PSMCFA}_2_2.psmc ${DIR}/${PSMCFA}
-psmc -p "1+1+1+1+25*2+4+6" -r5 -t15 -N25 -o ${OUTDIR}/${PSMCFA}_1_1_1_1.psmc ${DIR}/${PSMCFA}
+psmc -p "1+1+1+1+25*2+4+6" -r5 -t15 -N25 -o ${OUTDIR}/Chinese_CAN6.CanFam31_12x_1_1_1_1.psmc ${DIR}/Chinese_CAN6.CanFam31_12x.psmcfa
 
 
-psmc -p "1+1+1+1+25*2+4+6" -r5 -t15 -N25 -o ${OUTDIR}/Shanxi1Wolf_1_1_1_1.psmc ${DIR}/Shanxi1Wolf.psmcfa
+psmc -p "1+1+1+1+25*2+4+6" -r5 -t15 -N25 -o ${OUTDIR}/Chinese_CAN6.CanFam31_1_1_1_1.psmc ${DIR}/Chinese_CAN6.CanFam31.realigned.bam_autosomes.bam.psmcfa
+
 
 
 Shanxi1Wolf.psmcfa
