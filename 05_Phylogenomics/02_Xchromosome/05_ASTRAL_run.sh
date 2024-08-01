c7509fad8b39699aaed4d81be99a18417db50d23
Lauren Hennelly <lmhennelly@ucdavis.edu>
2024-08-01 16:00:19 -0400

    Update 05_ASTRAL_run.sh

--- 05_Phylogenomics/02_Xchromosome/05_ASTRAL_run.sh
+++ 05_Phylogenomics/02_Xchromosome/05_ASTRAL_run.sh
@@ -8,16 +8,19 @@
 
 #make a list of the treefiles 
 #cd /projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/phyfiles_chrX_July2024_all
-# cat *treefile > listtreefiles_nodogswithNA.txt
+# cat *treefile > listtreefiles_nodogswithNA_more_noEth.txt
+
+#cat /projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/xFINAL_chrX_July262026_input_July2024_nodogs_withNA_more_noEth/listtreefiles_nodogswithNA_more_noEth.txt listtreefiles_nodogswithNA.txt > listtreefiles_nodogswithNA_all.txt
+#head -n 500 listtreefiles_nodogswithNA_all.txt > listtreefiles_nodogswithNA_all_500.txt
 
 conda activate /projects/mjolnir1/apps/conda/astral-2.2
 module load jdk
 
 ## NO DOGS WITH NA 
-TREE=/projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/xFINAL_chrX_July262026_phyfiles_nodogs_withNA/listtreefiles_nodogswithNA.txt
-OUTDIR=/projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/xFINAL_chrX_July262026_phyfiles_nodogs_withNA_results
+TREE=/projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/xFINAL_chrX_July262026_phy_July2024_nodogs_withNA_noEth/listtreefiles_nodogswithNA_all_500.txt
+OUTDIR=/projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/xFINAL_chrX_July262026_phy_July2024_nodogs_withNA_noEth_results
 
-java -jar /projects/mjolnir1/people/crq857/Chapter2/bin/ASTER/ASTRAL/astral.5.7.8.jar -i ${TREE} -o ${OUTDIR}/July28_2024_chrX_nodogs_withNA.tre
+java -jar /projects/mjolnir1/people/crq857/Chapter2/bin/ASTER/ASTRAL/astral.5.7.8.jar -i ${TREE} -o ${OUTDIR}/Aug1_2024_chrX_nodogs_withNA.tre
 
 
 
