5746cdb9e51542acc6448a64afddfaa7ad84da11
Lauren Hennelly <lmhennelly@ucdavis.edu>
2024-08-02 15:36:59 -0400

    Update 02_IQtree_vcf2phy.sh

--- 05_Phylogenomics/04_lowrecombination/02_IQtree_vcf2phy.sh
+++ 05_Phylogenomics/04_lowrecombination/02_IQtree_vcf2phy.sh
@@ -9,11 +9,10 @@
 #conda activate /projects/mjolnir1/apps/conda/python-3.5.6 #this may differ for your cluster. The vcf2phylip needs a specific python version
 
 #Xchromosome
-XVCF=/projects/mjolnir1/people/crq857/Chapter2/06_Datasets/Xchr_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9_forASTRAl_lowrecombination.vcf.recode.vcf
-xVCF_noadmix=/projects/mjolnir1/people/crq857/Chapter2/06_Datasets/Xchr_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9_forASTRAl_lowrecombination_noPakistan.recode.vcf
-XOUTDIR=/projects/mjolnir1/people/crq857/Chapter2/06_Datasets/
+VCF=/projects/mjolnir1/people/crq857/Chapter2/06_Datasets/2gatk_chrX_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9_lowrecombination_noEthnodogs.recode.vcf 
+OUTDIR=/projects/mjolnir1/people/crq857/Chapter2/06_Datasets/
 
-python /projects/mjolnir1/people/crq857/Geneflow_Dogs/bin/vcf2phylip/vcf2phylip.py -i ${VCF} --output-folder ${OUTDIR} --output-prefix Xchr_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9_forASTRAl_lowrecombination -o Dhole_BerlinZoo
+python /projects/mjolnir1/people/crq857/Geneflow_Dogs/bin/vcf2phylip/vcf2phylip.py -i ${VCF} --output-folder ${OUTDIR} --output-prefix 2gatk_chrX_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9_lowrecombination_noEthnodogs -o AndeanFox
 python /projects/mjolnir1/people/crq857/Geneflow_Dogs/bin/vcf2phylip/vcf2phylip.py -i ${xVCF_noadmix} --output-folder ${OUTDIR} --output-prefix Xchr_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9_forASTRAl_lowrecombination_noadmixPak -o Dhole_BerlinZoo
 
 #You can download vcf2phylip here: https://github.com/edgardomortiz/vcf2phylip
