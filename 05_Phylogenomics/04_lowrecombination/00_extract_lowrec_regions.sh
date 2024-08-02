cf2f3ad6564f1d21608fdc659c9e4148801b9637
Lauren Hennelly <lmhennelly@ucdavis.edu>
2024-08-02 13:26:25 -0400

    Update 00_extract_lowrec_regions.sh

--- 05_Phylogenomics/04_lowrecombination/00_extract_lowrec_regions.sh
+++ 05_Phylogenomics/04_lowrecombination/00_extract_lowrec_regions.sh
@@ -9,19 +9,24 @@
 SITES=/home/crq857/projects/Chapter2/files/FINISHED_WHOLEGENOMEbelow0.2_FINAL_copy_fixed.bed
 VCF=/projects/mjolnir1/people/crq857/Chapter2/06_Datasets/Autosomes_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9_forASTRAl.recode.vcf
 OUTVCF=/projects/mjolnir1/people/crq857/Chapter2/06_Datasets/Autosomes_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9_forASTRAL_lowrecombination.vcf
+REMOVE=/home/crq857/projects/Chapter2/files/July26_taxaremove_onlydogs_badwolves.txt
+OUTVCF2=/projects/mjolnir1/people/crq857/Chapter2/06_Datasets/gatk_chrX_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9_lowrecombination_noEthnodogs
 
 module load perl
 module load vcftools
 
 #Autosomes
-vcftools --vcf ${VCF} --bed ${SITES} --out ${OUTVCF} --recode --keep-INFO-all
+#vcftools --vcf ${VCF} --bed ${SITES} --out ${OUTVCF} --recode --keep-INFO-all
+vcftools --vcf ${OUTVCF}.recode.vcf --remove ${REMOVE} --out ${OUTVCF2} --recode --keep-INFO-all
+
 
 ## X chromosome
-XSITES=/home/crq857/projects/Chapter2/files/FINISHED_WHOLEGENOMEbelow0.2_FINAL_copy_fixed.bed
-XVCF=/projects/mjolnir1/people/crq857/Chapter2/06_Datasets/Xchr_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9_forASTRAl.recode.vcf
-XOUTVCF=/projects/mjolnir1/people/crq857/Chapter2/06_Datasets/Xchr_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9_forASTRAl_lowrecombination.vcf
-
 module load perl
 module load vcftools
 
-vcftools --vcf ${XVCF} --bed ${XSITES} --out ${XOUTVCF} --recode --keep-INFO-all
+vcftools --gzvcf ${XVCF} --bed ${XSITES} --out ${XOUTVCF} --recode --keep-INFO-all
+
+REMOVE=/home/crq857/projects/Chapter2/files/July26_taxaremove_onlydogs_badwolves.txt
+OUTVCF=/projects/mjolnir1/people/crq857/Chapter2/06_Datasets/gatk_chrX_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9_lowrecombination_noEthnodogs
+
+vcftools --vcf ${XOUTVCF}.recode.vcf --remove ${REMOVE} --out ${OUTVCF} --recode --keep-INFO-all
