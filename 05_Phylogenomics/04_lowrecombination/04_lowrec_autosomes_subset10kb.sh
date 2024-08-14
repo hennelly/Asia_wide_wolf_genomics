ed0966911ebf65b01450412d0eced99063fe720a
Lauren Hennelly <lmhennelly@ucdavis.edu>
2024-08-14 17:34:38 -0400

    Update 04_lowrec_autosomes_subset10kb.sh

--- 05_Phylogenomics/04_lowrecombination/04_lowrec_autosomes_subset10kb.sh
+++ 05_Phylogenomics/04_lowrecombination/04_lowrec_autosomes_subset10kb.sh
@@ -3,9 +3,9 @@
 #SBATCH -c 1
 #SBATCH --time 01:40:00
 #SBATCH --mem-per-cpu 1G
-#SBATCH -o /home/crq857/projects/Geneflow_Dogs/slurmout/AWvcf2python_July2024_noadmix_chrX_noadmix.out
-#SBATCH -e /home/crq857/projects/Geneflow_Dogs/slurmout/AWvcf2python_July2024_noadmix_chrX_noadmix.err
-#SBATCH --array=1
+#SBATCH -o /home/crq857/projects/Geneflow_Dogs/slurmout/AWvcf2python_July2024_noadmix_chrX_noadmix_%A_%a.out
+#SBATCH -e /home/crq857/projects/Geneflow_Dogs/slurmout/AWvcf2python_July2024_noadmix_chrX_noadmix_%A_%a.err
+#SBATCH --array=1-606%10
 
 
 LINE1=$(sed "${SLURM_ARRAY_TASK_ID}q;d" /home/crq857/projects/Chapter2/scripts_PhylogeneticsJuly/lowrec_Autosomes_ranges.txt  | awk '{print $1}')
@@ -22,3 +22,5 @@ FILE2=/projects/mjolnir1/people/crq857/Chapter2/06_Datasets/Autosomes_filtered_n
 
 head -n ${LINE1} ${FILE2} > ${OUTDIR}/Autosomes_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9_forASTRAL_lowrecombination_AUG14_${NUMBER}
 tail -n ${LINE2} ${OUTDIR}/Autosomes_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9_forASTRAL_lowrecombination_AUG14_${NUMBER} > ${OUTDIR}/Autosomes_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9_forASTRAL_lowrecombination_AUG14_${NUMBER}_final
+
+cat header_autorec.txt ${OUTDIR}/Autosomes_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9_forASTRAL_lowrecombination_AUG14_${NUMBER}_final > ${OUTDIR}/Autosomes_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9_forASTRAL_lowrecombination_AUG14_${NUMBER}_final_header.vcf
