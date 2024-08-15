223c51034740199eff4920d9b9b30257bbabcae1
Lauren Hennelly <lmhennelly@ucdavis.edu>
2024-08-15 16:34:32 -0400

    Update 02_IQtree_vcf2phy.sh

--- 05_Phylogenomics/04_lowrecombination/02_IQtree_vcf2phy.sh
+++ 05_Phylogenomics/04_lowrecombination/02_IQtree_vcf2phy.sh
@@ -28,3 +28,23 @@ python /projects/mjolnir1/people/crq857/Geneflow_Dogs/bin/vcf2phylip/vcf2phylip.
 python /projects/mjolnir1/people/crq857/Geneflow_Dogs/bin/vcf2phylip/vcf2phylip.py -i ${VCF_noadmix} --output-folder ${OUTDIR} --output-prefix Autosomes_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9_forASTRAl_lowrecombination_noadmix_Pak -o Dhole_BerlinZoo
 
 
+# Low recombination autosomes
+#!/usr/bin/env bash
+#SBATCH --job-name=iqtree
+#SBATCH -c 1
+#SBATCH --time 11:40:00
+#SBATCH --mem-per-cpu 10G
+#SBATCH -o /home/crq857/projects/Chapter2/slurmout/lowrecombination_vcf2phy_noadmix_auto.out
+#SBATCH -e /home/crq857/projects/Chapter2/slurmout/lowrecombination_vcf2phy_noadmix_auto.err
+
+#conda activate /projects/mjolnir1/apps/conda/python-3.5.6 #this may differ for your cluster. The vcf2phylip needs a specific python version
+
+VCF=$(sed "${SLURM_ARRAY_TASK_ID}q;d" /projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/Aug14_lowrecautosomes/input/list2.txt) #this is the specific script to tell the cluster to run an array. 
+DIR=/projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/Aug14_lowrecautosomes/input
+OUTDIR=/projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/Aug14_lowrecautosomes/phyfiles
+
+python /projects/mjolnir1/people/crq857/Geneflow_Dogs/bin/vcf2phylip/vcf2phylip.py -i ${DIR}/${VCF} --output-folder ${OUTDIR} --output-prefix ${VCF}_phy -o Dhole_BerlinZoo
+
+
+list2.txt
+
