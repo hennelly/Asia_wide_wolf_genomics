4b3be6ca9ce31b28ee83dca3a61e1b0d3a01e879
Lauren Hennelly <lmhennelly@ucdavis.edu>
2024-08-17 22:09:08 -0400

    Update 07_Autosomes_comparisons.sh

--- 05_Phylogenomics/01_Autosomes/07_Autosomes_comparisons.sh
+++ 05_Phylogenomics/01_Autosomes/07_Autosomes_comparisons.sh
@@ -32,4 +32,25 @@ OUTFILE_noPakandclose=/projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics
 
 vcftools --vcf ${FILE} --remove ${REMOVE_noPakand2cose} --out ${OUTFILE_noPakandclose} --recode --keep-INFO-all
 
+
+
+## Subset 
+
  
+#!/usr/bin/env bash
+#SBATCH --job-name=astral
+#SBATCH -c 1
+#SBATCH --time 01:10:00
+#SBATCH --mem-per-cpu 1G
+#SBATCH -o /home/crq857/projects/Chapter2/slurmout_Oct/astral_extract_%A_%a.out
+#SBATCH -e /home/crq857/projects/Chapter2/slurmout_Oct/astral_extract_%A_%a.err
+#SBATCH --array=10-1000%10
+
+BEDFILE=$(sed "${SLURM_ARRAY_TASK_ID}q;d" /projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/bedfiles/listfiles.txt)
+DIR=/projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/Aug17
+VCF=/projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/xFINAL_VCFs_July262024/Autosomes_NO_ETHIOPIAN_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9_forASTRAl_nodogs_withNAwolves_noPakistan_noSWAsia_Auug17.recode.vcf
+
+module load bedtools
+
+# no dogs with NA wolves
+bedtools intersect -a ${VCF} -b /projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/bedfiles/${BEDFILE} > ${DIR}/July262024_Autosomes_nodogs_withNAwolves_noEthiopian_noPakistan_noSWAsia_Auug17_${BEDFILE}.vcf
