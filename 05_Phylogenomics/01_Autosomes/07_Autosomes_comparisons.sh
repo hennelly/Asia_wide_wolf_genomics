e9394fe6550773e24bb7a998764ed4f0453ebcbd
Lauren Hennelly <lmhennelly@ucdavis.edu>
2024-08-17 22:15:35 -0400

    Update 07_Autosomes_comparisons.sh

--- 05_Phylogenomics/01_Autosomes/07_Autosomes_comparisons.sh
+++ 05_Phylogenomics/01_Autosomes/07_Autosomes_comparisons.sh
@@ -3,8 +3,8 @@
 #SBATCH -c 1
 #SBATCH --time 1-11:10:00
 #SBATCH --mem-per-cpu 3G
-#SBATCH -o /home/crq857/projects/Chapter2/slurmout_Oct/astral_March2024_chrX_nodogsLadakj_withNA3.out
-#SBATCH -e /home/crq857/projects/Chapter2/slurmout_Oct/astral_March2024_chrX_nodogsLadakj_withNA3.err
+#SBATCH -o /home/crq857/projects/Chapter2/slurmout_Oct/astral_March2024_chrX_nodogsLadakj_withNA323.out
+#SBATCH -e /home/crq857/projects/Chapter2/slurmout_Oct/astral_March2024_chrX_nodogsLadakj_withNA332.err
 
 module load perl
 module load vcftools
@@ -19,38 +19,38 @@ REMOVE_noSWAsia=/home/crq857/projects/Chapter2/files/July26_taxaremove_onlydogs_
 OUTFILE_noSWAsia=/projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/xFINAL_VCFs_July262024/Autosomes_NO_ETHIOPIAN_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9_forASTRAl_nodogs_withNAwolves_noPakistan_noSWAsia_Auug17
 
 vcftools --vcf ${FILE} --remove ${REMOVE} --out ${OUTFILE_noSWAsia} --recode --keep-INFO-all
+grep -v "#" ${OUTFILE_noSWAsia}.recode.vcf > ${OUTFILE_noSWAsia}_noheader.recode.vcf
+grep "#" ${OUTFILE_noSWAsia}.recode.vcf > header_noSWAsia.txt
+shuf -n 1000000 ${OUTFILE_noSWAsia}_noheader.recode.vcf > ${OUTFILE_noSWAsia}_1millionrandom.vcf
+cat header_noSWAsia.txt ${OUTFILE_noSWAsia}_1millionrandom.vcf > ${OUTFILE_noSWAsia}_1millionrandom_header.vcf
 
 
+
+
+# remove Indian
 REMOVE_noIndia=/home/crq857/projects/Chapter2/files/July26_taxaremove_onlydogs_badwolves_Pakistan_Indian.txt
 OUTFILE_noIndia=/projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/xFINAL_VCFs_July262024/Autosomes_NO_ETHIOPIAN_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9_forASTRAl_nodogs_withNAwolves_noPakistan_noIndia_Auug17
 
 vcftools --vcf ${FILE} --remove ${REMOVE_noIndia} --out ${OUTFILE_noIndia} --recode --keep-INFO-all
+grep -v "#" ${OUTFILE_noIndia}.recode.vcf > ${OUTFILE_noIndia}_noheader.recode.vcf
+grep "#" ${OUTFILE_noIndia}.recode.vcf > header_nonoIndia.txt
+shuf -n 1000000 ${OUTFILE_noIndia}_noheader.recode.vcf > ${OUTFILE_noIndia}_1millionrandom.vcf
+cat header_nonoIndia.txt ${OUTFILE_noIndia}_1millionrandom.vcf > ${OUTFILE_noIndia}_1millionrandom_header.vcf
 
 
+
+
+
+# remove pakistan and 2 close individuals
 REMOVE_noPakand2cose=/home/crq857/projects/Chapter2/files/July26_taxaremove_onlydogs_badwolves_Pakistan_2closeSWAsia.txt
 OUTFILE_noPakandclose=/projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/xFINAL_VCFs_July262024/Autosomes_NO_ETHIOPIAN_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9_forASTRAl_nodogs_withNAwolves_noPakistan_nocloseSW_Auug17
 
 vcftools --vcf ${FILE} --remove ${REMOVE_noPakand2cose} --out ${OUTFILE_noPakandclose} --recode --keep-INFO-all
+grep -v "#" ${OUTFILE_noPakandclose}.recode.vcf > ${OUTFILE_noPakandclose}_noheader.recode.vcf
+grep "#" ${OUTFILE_noPakandclose}.recode.vcf > header_noPak.txt
+shuf -n 1000000 ${OUTFILE_noPakandclose}_noheader.recode.vcf > ${OUTFILE_noPakandclose}_1millionrandom.vcf
+cat header_noPak.txt ${OUTFILE_noPakandclose}_1millionrandom.vcf > ${OUTFILE_noPakandclose}_1millionrandom_header.vcf
 
 
 
-## Subset 
 
- 
-#!/usr/bin/env bash
-#SBATCH --job-name=astral
-#SBATCH -c 1
-#SBATCH --time 01:10:00
-#SBATCH --mem-per-cpu 1G
-#SBATCH -o /home/crq857/projects/Chapter2/slurmout_Oct/astral_extract_%A_%a.out
-#SBATCH -e /home/crq857/projects/Chapter2/slurmout_Oct/astral_extract_%A_%a.err
-#SBATCH --array=10-1000%10
-
-BEDFILE=$(sed "${SLURM_ARRAY_TASK_ID}q;d" /projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/bedfiles/listfiles.txt)
-DIR=/projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/Aug17
-VCF=/projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/xFINAL_VCFs_July262024/Autosomes_NO_ETHIOPIAN_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9_forASTRAl_nodogs_withNAwolves_noPakistan_noSWAsia_Auug17.recode.vcf
-
-module load bedtools
-
-# no dogs with NA wolves
-bedtools intersect -a ${VCF} -b /projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/bedfiles/${BEDFILE} > ${DIR}/July262024_Autosomes_nodogs_withNAwolves_noEthiopian_noPakistan_noSWAsia_Auug17_${BEDFILE}.vcf
