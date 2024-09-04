2f003d33cb627cdda47887742d516a23e7325fac
Lauren Hennelly <lmhennelly@ucdavis.edu>
2024-09-04 14:12:04 +0200

    Update 01_FEEMS.sh

--- 03_Geneflow_Analyses/02_FEEMS/01_FEEMS.sh
+++ 03_Geneflow_Analyses/02_FEEMS/01_FEEMS.sh
@@ -19,7 +19,8 @@ import feems
 
 ### ERROR FOR FEEMS 
 AttributeError: module 'numpy' has no attribute 'int'.
-`np.int` was a deprecated alias for the builtin `int`. To avoid this error in existing code, use `int` by itself. Doing this will not modify any behavior and is safe. When replacing `np.int`, you may wish to use e.g. `np.int64` or `np.int32` to specify the precision. If you wish to review your current use, check the release note link for additional information.
+`np.int` was a deprecated alias for the builtin `int`. To avoid this error in existing code, use `int` by itself. Doing this will not modify any behavior and is safe. 
+When replacing `np.int`, you may wish to use e.g. `np.int64` or `np.int32` to specify the precision. If you wish to review your current use, check the release note link for additional information.
 The aliases was originally deprecated in NumPy 1.20; for more details and guidance see the original release note at:
     https://numpy.org/devdocs/release/1.20.0-notes.html#deprecations
 ################ cant get past this step
