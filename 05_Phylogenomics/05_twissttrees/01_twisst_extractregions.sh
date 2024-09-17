8e124f9f03d2c69a0916c4e57339b77ee86f6239
Lauren Hennelly <lmhennelly@ucdavis.edu>
2024-09-17 14:55:49 -0400

    Update 01_twisst_extractregions.sh

--- 05_Phylogenomics/05_twissttrees/01_twisst_extractregions.sh
+++ 05_Phylogenomics/05_twissttrees/01_twisst_extractregions.sh
@@ -6,6 +6,8 @@
 #SBATCH -o /home/crq857/projects/Geneflow_Dogs/slurmout/bedtoolsrandom.out
 #SBATCH -e /home/crq857/projects/Geneflow_Dogs/slurmout/bedtoolsrandom.err
 
+## Extract genomic regions that have a twisst value of over 0.3 for topology 73
+
 VCF=/projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/xFINAL_VCFs_July262024/Autosomes_NO_ETHIOPIAN_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9_forASTRAl_nodogs_withNAwolves.recode.vcf
 BED=/projects/mjolnir1/people/crq857/Chapter2/twisstresults/bedfile.bed
 OUTVCF=/projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/xFINAL_VCFs_July262024/Autosomes_NO_ETHIOPIAN_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9_forASTRAl_nodogs_withNAwolves_twisst0.3
