635ccb09715fa9e4554b373a8b836418ea0f527e
Lauren Hennelly <lmhennelly@ucdavis.edu>
2024-09-17 15:05:50 -0400

    Update and rename 02_inferIQtree_topo73regions_withPakistan.sh to 02_inferIQtree_topo73regions_bothtrees.sh

--- 05_Phylogenomics/05_twissttrees/02_inferIQtree_topo73regions_bothtrees.sh
+++ 05_Phylogenomics/05_twissttrees/02_inferIQtree_topo73regions_bothtrees.sh
@@ -7,7 +7,9 @@
 #SBATCH -e /home/crq857/projects/Geneflow_Dogs/slurmout/vcf2python_fullgenome_noadmixd_.err
 #SBATCH --array=1
 
-#add header
+##############
+#add header ##
+##############
 OUTVCF=/projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/xFINAL_VCFs_July262024/Autosomes_NO_ETHIOPIAN_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9_forASTRAl_nodogs_withNAwolves.recode.vcf
 
 grep "#" ${OUTVCF} > header_fortwisst.txt
@@ -15,7 +17,9 @@ grep "#" ${OUTVCF} > header_fortwisst.txt
 VCF=/projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/xFINAL_VCFs_July262024/Autosomes_NO_ETHIOPIAN_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9_forASTRAl_nodogs_withNAwolves_twisst0.3
 cat header_fortwisst.txt ${VCF} > /projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/xFINAL_VCFs_July262024/Autosomes_NO_ETHIOPIAN_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9_forASTRAl_nodogs_withNAwolves_twisst0.3_witheader.vcf
 
-# Make a dataset without pakistan
+####################################
+# Make a dataset without pakistan ##
+####################################
 VCF2=/projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/xFINAL_VCFs_July262024/Autosomes_NO_ETHIOPIAN_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9_forASTRAl_nodogs_withNAwolves_twisst0.3_witheader.vcf
 REMOVE=/home/crq857/projects/Chapter2/files/July26_taxaremove_onlydogs_badwolves_Pakistan.txt
 OUTVCF2=/projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/xFINAL_VCFs_July262024/Autosomes_NO_ETHIOPIAN_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9_forASTRAl_nodogs_withNAwolves_twisst0.3_witheader_noPakistan.vcf
@@ -25,7 +29,9 @@ module load vcftools
 
 vcftools --vcf ${VCF2} --remove ${REMOVE} --out ${OUTVCF2} --recode --keep-INFO-all
 
-#convert to phy
+#####################
+### convert to phy ##
+######################
 VCF2=/projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/xFINAL_VCFs_July262024/Autosomes_NO_ETHIOPIAN_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9_forASTRAl_nodogs_withNAwolves_twisst0.3_witheader.vcf
 OUTDIR=/projects/mjolnir1/people/crq857/Chapter2/twisstresults
 OUTVCF2=/projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/xFINAL_VCFs_July262024/Autosomes_NO_ETHIOPIAN_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9_forASTRAl_nodogs_withNAwolves_twisst0.3_witheader_noPakistan.vcf.recode.vcf
@@ -33,7 +39,9 @@ OUTVCF2=/projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/xFINAL_VCFs_J
 python /projects/mjolnir1/people/crq857/Geneflow_Dogs/bin/vcf2phylip/vcf2phylip.py -i ${VCF2} --output-folder ${OUTDIR} --output-prefix Autosomes_regions_above0.3_IndianandSWAsia_header_phy -o AndeanFox
 python /projects/mjolnir1/people/crq857/Geneflow_Dogs/bin/vcf2phylip/vcf2phylip.py -i ${VCFnoPak} --output-folder ${OUTDIR} --output-prefix Autosomes_regions_above0.3_IndianandSWAsia_header_noPak_phy -o AndeanFox
 
-#run Iqtree
+##################
+### run Iqtree ###
+##################
 PHY=/projects/mjolnir1/people/crq857/Chapter2/twisstresults/Autosomes_regions_above0.3_IndianandSWAsia_header_phy.min4.phy
 
 /home/crq857/bin/iqtree-1.6.12-Linux/bin/iqtree -s ${PHY} -bb 1000 -nt AUTO -m MFP
@@ -42,3 +50,10 @@ PHY=/projects/mjolnir1/people/crq857/Chapter2/twisstresults/Autosomes_regions_ab
 
 /home/crq857/bin/iqtree-1.6.12-Linux/bin/iqtree -s ${PHY} -bb 1000 -nt AUTO -m MFP
 
+
+scp -r crq857@mjolnirgate.unicph.domain:/projects/mjolnir1/people/crq857/Chapter2/twisstresults/Autosomes_regions_above0.3_IndianandSWAsia_header_noPak_phy.min4.phy.treefile ~/Desktop
+scp -r crq857@mjolnirgate.unicph.domain:/projects/mjolnir1/people/crq857/Chapter2/twisstresults/Autosomes_regions_above0.3_IndianandSWAsia_header_phy.min4.phy.treefile ~/Desktop
+
+
+/projects/mjolnir1/people/crq857/Chapter2/twisstresults/Autosomes_regions_above0.3_IndianandSWAsia_header_noPak_phy.min4.phy.treefile
+/projects/mjolnir1/people/crq857/Chapter2/twisstresults/Autosomes_regions_above0.3_IndianandSWAsia_header_phy.min4.phy.treefile
