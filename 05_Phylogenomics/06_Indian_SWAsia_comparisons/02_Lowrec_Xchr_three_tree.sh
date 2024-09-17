6ddfa2eb3f441e742c1ae767c856c49efddf7fc8
Lauren Hennelly <lmhennelly@ucdavis.edu>
2024-09-17 17:11:19 -0400

    Create 02_Lowrec_Xchr_three_tree.sh

---
+++ 05_Phylogenomics/06_Indian_SWAsia_comparisons/02_Lowrec_Xchr_three_tree.sh
@@ -0,0 +1,7 @@
+
+
+
+VCFlowrec=/projects/mjolnir1/people/crq857/Chapter2/06_Datasets/2gatk_chrX_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9_lowrecombination_noEthnodogs_header_Aug15.recode.vcf
+
+REMOVE_noIndia=/home/crq857/projects/Chapter2/files/July26_taxaremove_onlydogs_badwolves_Pakistan_Indian.txt
+
