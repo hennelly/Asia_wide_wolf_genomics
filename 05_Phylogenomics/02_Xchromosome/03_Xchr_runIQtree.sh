db6625af4a4a6dc276e55463a9de0c9b71237df7
Lauren Hennelly <lmhennelly@ucdavis.edu>
2025-01-22 16:50:20 -0500

    Update and rename 04_ASTRAL_runIQtree.sh to 03_Xchr_runIQtree.sh

---
+++ 05_Phylogenomics/02_Xchromosome/03_Xchr_runIQtree.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+#SBATCH --job-name=iqtree
+#SBATCH -c 1
+#SBATCH --time 01:40:00
+#SBATCH --mem-per-cpu 1G
+#SBATCH -o /home/crq857/projects/Chapter2/slurmout/iqtree_Xchr_noadmix.out
+#SBATCH -e /home/crq857/projects/Chapter2/slurmout/iqtree_Xchr_noadmix.err
+
+#All individuals
+PHY=/projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics_Jan2025/FINAL_VCFs_2025/chrX_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9_forASTRAl_nodogs_Jan2025_phy.min4.phy
+
+/home/crq857/bin/iqtree-1.6.12-Linux/bin/iqtree -s ${PHY} -bb 1000 -nt AUTO
+
+
+chrX_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9_forASTRAl_nodogs_Jan2025_phy.min4.phy
+
+### NO ADMIXED
+
+PHY=/projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics_Jan2025/FINAL_VCFs_2025/chrX_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9_forASTRAl_nodogs_Jan2025_phy.min4.phy
+
+/home/crq857/bin/iqtree-1.6.12-Linux/bin/iqtree -s ${PHY} -bb 1000 -nt AUTO
+
+
+
+
+
+
