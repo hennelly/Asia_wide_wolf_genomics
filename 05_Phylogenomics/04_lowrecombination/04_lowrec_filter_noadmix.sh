2d551a30234deec9cac95546c668692959898873
Lauren Hennelly <lmhennelly@ucdavis.edu>
2025-02-03 16:50:07 -0500

    Update 04_lowrec_filter_noadmix.sh

--- 05_Phylogenomics/04_lowrecombination/04_lowrec_filter_noadmix.sh
+++ 05_Phylogenomics/04_lowrecombination/04_lowrec_filter_noadmix.sh
@@ -6,7 +6,7 @@
 #SBATCH -o /home/crq857/projects/Chapter2/slurmout/lowrecombination_removeadmixWestAsia.out
 #SBATCH -e /home/crq857/projects/Chapter2/slurmout/lowrecombination_removeadmixWestAsia.err
 
-VCF=/projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics_Jan2025/FINAL_VCFs_2025/chrX_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9_forASTRAl_nodogs_Jan2025_lowrecombination.vcf
+VCF=/projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics_Jan2025/FINAL_VCFs_2025/chrX_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9_forASTRAl_nodogs_Jan2025_lowrecombination.vcf.recode.vcf
 REMOVE=/home/crq857/projects/Chapter2/files/removeadmix.txt
 XOUTVCF=/projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics_Jan2025/FINAL_VCFs_2025/chrX_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9_forASTRAl_nodogs_Jan2025_lowrecombination_noPakLadakh.vcf
 
@@ -18,7 +18,7 @@ vcftools --vcf ${VCF} --remove ${REMOVE} --out ${XOUTVCF} --recode --keep-INFO-a
 
 ## NO PAKISTAN AND WEST ASIA 
 
-VCF=/projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics_Jan2025/FINAL_VCFs_2025/chrX_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9_forASTRAl_nodogs_Jan2025_lowrecombination.vcf
+VCF=/projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics_Jan2025/FINAL_VCFs_2025/chrX_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9_forASTRAl_nodogs_Jan2025_lowrecombination.vcf.recode.vcf
 REMOVE=/home/crq857/projects/Chapter2/files/removeadmix_WestAsia.txt
 XOUTVCFwest=/projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics_Jan2025/FINAL_VCFs_2025/chrX_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9_forASTRAl_nodogs_Jan2025_lowrecombination_noPakLadakhwestAsia.vcf
 
