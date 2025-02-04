637ce2a189d81c4d5d13268ac171efe90191443b
Lauren Hennelly <lmhennelly@ucdavis.edu>
2025-02-03 23:13:23 -0500

    Update 05_ASTRAL_runIQtree.sh

--- 05_Phylogenomics/01_Autosomes/05_ASTRAL_runIQtree.sh
+++ 05_Phylogenomics/01_Autosomes/05_ASTRAL_runIQtree.sh
@@ -1,27 +1,21 @@
 #!/usr/bin/env bash
-#SBATCH --job-name=ASTRAL
+#SBATCH --job-name=iqtreeauto
 #SBATCH -c 1
-#SBATCH --time 11:40:00
+#SBATCH --time 1-11:40:00
 #SBATCH --mem-per-cpu 1G
-#SBATCH -o /home/crq857/projects/Geneflow_Dogs/slurmout/iqtree_fullgenome_run_%A_%a.out
-#SBATCH -e /home/crq857/projects/Geneflow_Dogs/slurmout/iqtree_fullgenome_run_%A_%a.err
-#SBATCH --array=1-20%15
+#SBATCH -o /home/crq857/projects/Chapter2/slurmout/iqtree_fullgenome_run_%A_%a.out
+#SBATCH -e /home/crq857/projects/Chapter2/slurmout/iqtree_fullgenome_run_%A_%a.err
+#SBATCH --array=1-992%15
 
 #conda activate /projects/mjolnir1/apps/conda/python-3.5.6
-#cd /projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/x_FINAL_July262024_phy_autosomes_noadmix_noEth
-#ls  *phy > FINAL_list_autosomes_noadmix.txt_phy_noEthmore.txt
+#cd /projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics_Jan2025/Astral_Autosomes/VCFs_final
+#ls  *phy > FINAL_list_autosomes_Feb3.txt
 
 ### no dogs, with NA
-PHY=$(sed "${SLURM_ARRAY_TASK_ID}q;d" /projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/x_FINAL_July262024_input_autosomes_nodogswithNA_noEth_more/FINAL_list_autosomes_noadmix.txt_phy_noEthmore.txt)
+PHY=$(sed "${SLURM_ARRAY_TASK_ID}q;d" /projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics_Jan2025/Astral_Autosomes/VCFs_final/FINAL_list_autosomes_Feb3.txt)
 
-/home/crq857/bin/iqtree-1.6.12-Linux/bin/iqtree -s  /projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/x_FINAL_July262024_input_autosomes_nodogswithNA_noEth_more/${PHY} -bb 1000 -nt AUTO
+/home/crq857/bin/iqtree-1.6.12-Linux/bin/iqtree -s  /projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics_Jan2025/Astral_Autosomes/VCFs_final/${PHY} -bb 1000 -nt AUTO
 
-## no admix
-
-#ls  *phy > FINAL_list_autosomes_noadmixtxt_phy.txt
-PHY=$(sed "${SLURM_ARRAY_TASK_ID}q;d" /projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/x_FINAL_July262024_phy_autosomes_noadmix_noEth/FINAL_list_autosomes_noadmix.txt_phy_noEth.txt)
-
-/home/crq857/bin/iqtree-1.6.12-Linux/bin/iqtree -s  /projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics/x_FINAL_July262024_phy_autosomes_noadmix_noEth/${PHY} -bb 1000 -nt AUTO
 
 
 
