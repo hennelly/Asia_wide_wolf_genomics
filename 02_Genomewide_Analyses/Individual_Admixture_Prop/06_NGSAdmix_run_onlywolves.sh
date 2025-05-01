9259397a163d3a38bb6d24660f8924c9e830b808
Lauren Hennelly <lmhennelly@ucdavis.edu>
2025-04-30 21:25:16 -0400

    Update 06_NGSAdmix_run_onlywolves.sh

--- 02_Genomewide_Analyses/Individual_Admixture_Prop/06_NGSAdmix_run_onlywolves.sh
+++ 02_Genomewide_Analyses/Individual_Admixture_Prop/06_NGSAdmix_run_onlywolves.sh
@@ -1,3 +1,17 @@
+#!/usr/bin/env bash
+#SBATCH --job-name=merge
+#SBATCH -c 1
+#SBATCH --time 15:00:00
+#SBATCH --mem-per-cpu 1G
+#SBATCH -o /home/crq857/projects/Asianwolves_revision/slurmout/merge_ngsadmix.out
+#SBATCH -e /home/crq857/projects/Asianwolves_revision/slurmout/merge_ngsadmix.err
 
+cd /projects/psg/people/crq857/Asianwolves_revision/01_GenomewideAnalyses/Admixture
 
-IN=/projects/mjolnir1/people/crq857/Chapter2/02_GenomewideAnalyses/Beaglefiles_PCA/Asianwolves_PCAdmix_Dec26_autosomes.beagle.gz
+cp Graywolves_Apr25_2025_NGSAdmix_chr1.beagle.gz Graywolves_Apr25_2025_NGSAdmix_autosomes.beagle.gz
+
+for CHR in `seq 2 38`
+do
+  gunzip -c Graywolves_Apr25_2025_NGSAdmix_chr${CHR}.beagle.gz | sed 1d | gzip -c
+done >> Graywolves_Apr25_2025_NGSAdmix_autosomes.beagle.gz
+
