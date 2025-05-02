179bc3c61027e6045331d540670205ea83f8868b
Lauren Hennelly <lmhennelly@ucdavis.edu>
2025-05-01 23:23:57 -0400

    Create 09_Quantify_recombination_rate.sh

---
+++ 05_Phylogenomics/01_Autosomes/09_Quantify_recombination_rate.sh
@@ -0,0 +1,8 @@
+
+
+
+LOWSITES=/home/crq857/projects/Chapter2/files/FINISHED_WHOLEGENOMEbelow0.2_FINAL_copy_fixed.bed
+
+MEDSITES=/projects/psg/people/crq857/Chapter2/recomb_files/FINISHED_WHOLEGENOME_between0.2to2_fixed2.bed
+
+HIGHSITES=/projects/psg/people/crq857/Chapter2/recomb_files/FINISHED_WHOLEGENOME_above2_fixed2.bed
