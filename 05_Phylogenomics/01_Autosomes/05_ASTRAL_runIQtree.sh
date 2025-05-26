a1fff5acdbd6662075e6d7c83e6be21c9739b20b
Lauren Hennelly <lmhennelly@ucdavis.edu>
2025-05-26 16:35:26 -0400

    Update 05_ASTRAL_runIQtree.sh

--- 05_Phylogenomics/01_Autosomes/05_ASTRAL_runIQtree.sh
+++ 05_Phylogenomics/01_Autosomes/05_ASTRAL_runIQtree.sh
@@ -1,20 +1,17 @@
 #!/usr/bin/env bash
-#SBATCH --job-name=iqtreeauto
-#SBATCH -c 1
-#SBATCH --time 1-11:40:00
-#SBATCH --mem-per-cpu 1G
-#SBATCH -o /home/crq857/projects/Chapter2/slurmout/iqtree_fullgenome_run_%A_%a.out
-#SBATCH -e /home/crq857/projects/Chapter2/slurmout/iqtree_fullgenome_run_%A_%a.err
-#SBATCH --array=1-992%15
+#SBATCH --partition=cpuqueue
+#SBATCH --qos=normal
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=1G
+#SBATCH --array=1-11
+#SBATCH --output=/home/crq857/projects/Asianwolves_revision/slurmout/zAutosomes_astral_%A_%a.out
+#SBATCH --error=/home/crq857/projects/Asianwolves_revision/slurmout/zAutosomes_astral_%A_%a.err
+#SBATCH --time=02:00:00
 
-#conda activate /projects/mjolnir1/apps/conda/python-3.5.6
-#cd /projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics_Jan2025/Astral_Autosomes/VCFs_final
-#ls  *phy > FINAL_list_autosomes_Feb3.txt
+PHY=$(sed "${SLURM_ARRAY_TASK_ID}q;d" /projects/psg/people/crq857/Chapter2/05_Phylogenomics/ASTRAL_vcf_more/FINAL_list_autosomes_May202025.txt)
+DIR=/projects/psg/people/crq857/Chapter2/05_Phylogenomics/ASTRAL_vcf_more
 
-### no dogs, with NA
-PHY=$(sed "${SLURM_ARRAY_TASK_ID}q;d" /projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics_Jan2025/Astral_Autosomes/VCFs_final/FINAL_list_autosomes_Feb3.txt)
-
-/home/crq857/bin/iqtree-1.6.12-Linux/bin/iqtree -s  /projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics_Jan2025/Astral_Autosomes/VCFs_final/${PHY} -bb 1000 -nt AUTO
+/home/crq857/bin/iqtree-1.6.12-Linux/bin/iqtree -s ${DIR}/${PHY} -bb 1000 -nt AUTO
 
 
 
