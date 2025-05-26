76a0c3ad14f50519cd683b0c2c767fd5a88180e7
Lauren Hennelly <lmhennelly@ucdavis.edu>
2025-05-26 16:34:32 -0400

    Update 02_ASTRAL_selectrandom.sh

--- 05_Phylogenomics/01_Autosomes/02_ASTRAL_selectrandom.sh
+++ 05_Phylogenomics/01_Autosomes/02_ASTRAL_selectrandom.sh
@@ -12,21 +12,9 @@ module load bedtools
 
 bedtools random -g ${GENOME} -l 20000 -n 5000 
 
-#Grab X chromosome regions
-grep "chrX" /home/crq857/projects/Geneflow_Dogs/slurmout/bedtoolsrandom_again.out >  /home/crq857/projects/Geneflow_Dogs/slurmout/bedtoolsrandom_again_chrX.out #44 regions
-grep "chrX" /home/crq857/projects/Geneflow_Dogs/slurmout/bedtoolsrandom_again2.out >  /home/crq857/projects/Geneflow_Dogs/slurmout/bedtoolsrandom_again_chrX2.out #42 regions
-cat   /home/crq857/projects/Geneflow_Dogs/slurmout/bedtoolsrandom_again_chrX.out  /home/crq857/projects/Geneflow_Dogs/slurmout/bedtoolsrandom_again_chrX2.out > more_randomregions_chrX.txt
-
 #clean up output - take only first three columns
-
 awk '{print $1,$2,$3}' randomregion_list_auto.bed > randomregion_list_auto2.bed
 tr ' ' \\t < randomregion_list_auto2.bed > randomregion_list_auto2_tabbed.bed
 
 #write each line of bed file as its own file
-
-FILE=/home/crq857/projects/Chapter2/scripts_PhylogeneticsJuly/more_randomregions_chrX_tabbed.bed
-
-awk '!/^$/{print  > "File_"NR".txt" }' ${FILE}
-
-
 awk '!/^$/{print  > "File_"NR".txt" }' randomregion_list_auto2_tabbed.bed
