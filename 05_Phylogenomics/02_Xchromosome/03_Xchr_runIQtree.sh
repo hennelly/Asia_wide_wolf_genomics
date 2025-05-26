ad1d258f4a15200e64b167f13170a180ed32d035
Lauren Hennelly <lmhennelly@ucdavis.edu>
2025-05-26 16:00:06 -0400

    Update 03_Xchr_runIQtree.sh

--- 05_Phylogenomics/02_Xchromosome/03_Xchr_runIQtree.sh
+++ 05_Phylogenomics/02_Xchromosome/03_Xchr_runIQtree.sh
@@ -1,25 +1,31 @@
 #!/usr/bin/env bash
-#SBATCH --job-name=iqtree
-#SBATCH -c 1
-#SBATCH --time 11:40:00
-#SBATCH --mem-per-cpu 10G
-#SBATCH -o /home/crq857/projects/Chapter2/slurmout/iqtree_Xchr_admix.out
-#SBATCH -e /home/crq857/projects/Chapter2/slurmout/iqtree_Xchr_admix.err
+#SBATCH --partition=cpuqueue
+#SBATCH --qos=normal
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=100G
+#SBATCH --output=/home/crq857/projects/Asianwolves_revision/slurmout/Xchr_filter2.out
+#SBATCH --error=/home/crq857/projects/Asianwolves_revision/slurmout/Xchr_filter2.err
+#SBATCH --time=2-10:00:00
 
-#All individuals
-PHY=/projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics_Jan2025/FINAL_VCFs_2025/chrX_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9_forASTRAl_nodogs_Jan2025_phy.min4.phy
+module load perl
+module load vcftools
 
-/home/crq857/bin/iqtree-1.6.12-Linux/bin/iqtree -s ${PHY} -bb 1000 -nt AUTO
+VCF=/projects/psg/people/crq857/Chapter2/05_Phylogenomics/VCFs_July2024/chrX_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9_forASTRAl_nodogs_withNA.recode.vcf
 
+REMOVE=/home/crq857/projects/Asianwolves_revision/files/remove_Xchr_phylogeny2.txt
+OUT=/projects/psg/people/crq857/Asianwolves_revision/03_Phylogenomics/REVISION_May17_2025_chrX_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9_forASTRAl_nodogs
 
-chrX_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9_forASTRAl_nodogs_Jan2025_phy.min4.phy
+vcftools --vcf ${VCF} --remove ${REMOVE} --recode --recode-INFO-all --out ${OUT}
 
-### NO ADMIXED
+VCFdone=/projects/psg/people/crq857/Asianwolves_revision/03_Phylogenomics/REVISION_May17_2025_chrX_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9_forASTRAl_nodogs.recode.vcf
 
-PHY=/projects/mjolnir1/people/crq857/Chapter2/05_Phylogenomics_Jan2025/FINAL_VCFs_2025/chrX_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9_forASTRAl_nodogs_noLadakhPakistanTajWestAsia_Jan2025_phy.min4.phy
+OUTDIR=/projects/psg/people/crq857/Asianwolves_revision/03_Phylogenomics/
 
-/home/crq857/bin/iqtree-1.6.12-Linux/bin/iqtree -s ${PHY} -bb 1000 -nt AUTO
+python /projects/psg/people/crq857/bin/vcf2phylip/vcf2phylip.py -i ${VCFdone} --output-folder ${OUTDIR} --output-prefix REVISION_MAY_17_2025_Xchr_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9_forASTRAl -o AndeanFox
 
+PHY=/projects/psg/people/crq857/Asianwolves_revision/03_Phylogenomics/REVISION_MAY_17_2025_Xchr_filtered_noindels_noastrick_diploid_minQ30_biallelic_maxmiss0.9_forASTRAl.min4.phy
+
+/home/crq857/bin/iqtree-1.6.12-Linux/bin/iqtree -s ${PHY} -bb 1000 -nt AUTO -m MFP
 
 
 
