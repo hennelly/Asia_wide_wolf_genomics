a9f45c6086958fd7b74726689df4acf24f058e94
Lauren Hennelly <lmhennelly@ucdavis.edu>
2025-06-14 20:37:45 +0900

    Update 03_NGSAdmix_run.sh

--- 02_Genomewide_Analyses/Individual_Admixture_Prop/03_NGSAdmix_run.sh
+++ 02_Genomewide_Analyses/Individual_Admixture_Prop/03_NGSAdmix_run.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
-#SBATCH --job-name=NGSAdmix
+#SBATCH --job-name=angsd
 #SBATCH -c 1
-#SBATCH --time 2-00:00:00
-#SBATCH --mem-per-cpu 30G
-#SBATCH -o /home/crq857/projects/Chapter2/slurmout/angsd_NGSadmix_test_%A_%a.out
-#SBATCH -e /home/crq857/projects/Chapter2/slurmout/angsd_NGSadmix_test_%A_%a.err
-#SBATCH --array=1-12%10
+#SBATCH --time 4-12:00:00
+#SBATCH --mem-per-cpu 50G
+#SBATCH -o /home/crq857/projects/Asianwolves_revision/slurmout/angsd_Apr25_wolvesNGSadmix_%A_%a.out
+#SBATCH -e /home/crq857/projects/Asianwolves_revision/slurmout/angsd_Apr25_wolvesNGSadmix_%A_%a.err
+#SBATCH --array=1-12%5
 
 K=$(sed "${SLURM_ARRAY_TASK_ID}q;d" /home/crq857/projects/Chapter2/files/NGAdmixlist.txt | cut -f1)
 
-IN=/projects/mjolnir1/people/crq857/Chapter2/02_GenomewideAnalyses/Beaglefiles_Jan22/Allcanids_Jan22_2025_NGSAdmix_autosomes.beagle.gz
+IN=/projects/psg/people/crq857/Asianwolves_revision/01_GenomewideAnalyses/Admixture_June2025_wolves/Graywolves_June12_2025_NGSAdmix_autosomes.beagle.gz
 
-/projects/mjolnir1/people/crq857/Chapter2/bin/NGSAdmix/NGSadmix -likes ${IN} -K ${K} -o /projects/mjolnir1/people/crq857/Chapter2/02_GenomewideAnalyses/Beaglefiles_Jan22/Autosomes_DONE_allcanids_Feb3_NGSAdmix_${K} -P 10  
+/projects/psg/people/crq857/Chapter2/bin/NGSAdmix/NGSadmix -likes ${IN} -K ${K} -o /projects/psg/people/crq857/Asianwolves_revision/01_GenomewideAnalyses/Admixture_June2025_wolves/Graywolves_June12_2025_NGSAdmix_autosomes_K${K} -P 10  
 
 
 
