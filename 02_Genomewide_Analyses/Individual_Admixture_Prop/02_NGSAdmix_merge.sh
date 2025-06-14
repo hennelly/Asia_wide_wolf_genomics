748f3e32b2934749a1705c98a291b4068fc42419
Lauren Hennelly <lmhennelly@ucdavis.edu>
2025-06-14 20:39:14 +0900

    Update 02_NGSAdmix_merge.sh

--- 02_Genomewide_Analyses/Individual_Admixture_Prop/02_NGSAdmix_merge.sh
+++ 02_Genomewide_Analyses/Individual_Admixture_Prop/02_NGSAdmix_merge.sh
@@ -17,4 +17,25 @@ done >> Graywolves_June12_2025_NGSAdmix_autosomes.beagle.gz
 
 
 
+#!/usr/bin/env bash
+#SBATCH --job-name=angsd
+#SBATCH -c 1
+#SBATCH --time 12:00:00
+#SBATCH --mem-per-cpu 10G
+#SBATCH -o /home/crq857/projects/Asianwolves_revision/slurmout/combine.out
+#SBATCH -e /home/crq857/projects/Asianwolves_revision/slurmout/combine.err
+
+cp Allcanids_June12_2025_NGSAdmix_chr1.beagle.gz Allcanids_June12_2025_NGSAdmix_autosomes.beagle.gz
+
+for CHR in `seq 2 38`
+do
+  gunzip -c Allcanids_June12_2025_NGSAdmix_chr${CHR}.beagle.gz | sed 1d | gzip -c
+done >> Allcanids_June12_2025_NGSAdmix_autosomes.beagle.gz
+
+
+
+
+
+
+
 
