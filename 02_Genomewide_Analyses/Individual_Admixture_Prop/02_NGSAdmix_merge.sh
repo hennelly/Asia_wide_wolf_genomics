a7abbfa7612e92be166ea21c86c5a54ae16f2c40
Lauren Hennelly <lmhennelly@ucdavis.edu>
2025-06-14 16:19:29 +0900

    Update 02_NGSAdmix_merge.sh

--- 02_Genomewide_Analyses/Individual_Admixture_Prop/02_NGSAdmix_merge.sh
+++ 02_Genomewide_Analyses/Individual_Admixture_Prop/02_NGSAdmix_merge.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
-#SBATCH --job-name=merge
+#SBATCH --job-name=angsd
 #SBATCH -c 1
-#SBATCH --time 15:00:00
-#SBATCH --mem-per-cpu 1G
-#SBATCH -o /home/crq857/projects/Chapter2/slurmout/merge_ngsadmix.out
-#SBATCH -e /home/crq857/projects/Chapter2/slurmout/merge_ngsadmix.err
+#SBATCH --time 12:00:00
+#SBATCH --mem-per-cpu 10G
+#SBATCH -o /home/crq857/projects/Asianwolves_revision/slurmout/combine.out
+#SBATCH -e /home/crq857/projects/Asianwolves_revision/slurmout/combine.err
 
-cd /projects/mjolnir1/people/crq857/Chapter2/02_GenomewideAnalyses/Beaglefiles_Jan22 
-
-cp Allcanids_Jan22_2025_NGSAdmix_chr1.beagle.gz Allcanids_Jan22_2025_NGSAdmix_autosomes.beagle.gz
+cp Graywolves_June12_2025_NGSAdmix_chr1.beagle.gz Graywolves_June12_2025_NGSAdmix_autosomes.beagle.gz
 
 for CHR in `seq 2 38`
 do
-  gunzip -c Allcanids_Jan22_2025_NGSAdmix_chr${CHR}.beagle.gz | sed 1d | gzip -c
-done >> Allcanids_Jan22_2025_NGSAdmix_autosomes.beagle.gz
+  gunzip -c Graywolves_June12_2025_NGSAdmix_chr${CHR}.beagle.gz | sed 1d | gzip -c
+done >> Graywolves_June12_2025_NGSAdmix_autosomes.beagle.gz
+
+
+
