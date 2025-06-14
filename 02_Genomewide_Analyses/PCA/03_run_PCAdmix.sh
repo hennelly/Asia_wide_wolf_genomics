83a3b49719ac43e204741b646e04e106755921ba
Lauren Hennelly <lmhennelly@ucdavis.edu>
2025-06-14 16:51:17 +0900

    Update 03_run_PCAdmix.sh

--- 02_Genomewide_Analyses/PCA/03_run_PCAdmix.sh
+++ 02_Genomewide_Analyses/PCA/03_run_PCAdmix.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
-#SBATCH --job-name=PCAngsd
+#SBATCH --job-name=pca
 #SBATCH -c 1
 #SBATCH --time 12:00:00
-#SBATCH --mem-per-cpu 5G
-#SBATCH -o /home/crq857/projects/Chapter2/slurmout_Oct/PCAngsd.out
-#SBATCH -e /home/crq857/projects/Chapter2/slurmout_Oct/PCAngsd.err
+#SBATCH --mem-per-cpu 10G
+#SBATCH -o /home/crq857/projects/Asianwolves_revision/slurmout/pca.out
+#SBATCH -e /home/crq857/projects/Asianwolves_revision/slurmout/pca.err
 
-#conda activate /projects/mjolnir1/apps/conda/pcangsd-1.10
+module load pcangsd
 
-IN=/projects/mjolnir1/people/crq857/Chapter2/02_GenomewideAnalyses/Beaglefiles_PCA/Asianwolves_PCAdmix_Dec26_autosomes.beagle.gz
-OUTDIR=/projects/mjolnir1/people/crq857/Chapter2/02_GenomewideAnalyses/PCA
+IN=/projects/psg/people/crq857/Chapter2/02_GenomewideAnalyses/Beaglefiles_PCA_June2025/Asianwolves_PCAdmix_June2025_autosomes.beagle.gz
+OUTDIR=/projects/psg/people/crq857/Chapter2/02_GenomewideAnalyses/Beaglefiles_PCA_June2025
 
 # Estimate covariance matrix for pca
-pcangsd -b ${IN} -e 2 -o ${OUTDIR}/output_Dec29_Asianwolf.txt -t 2
+pcangsd -b ${IN} -e 2 -o ${OUTDIR}/output_June14_2025_Asianwolf.txt -t 2
+
+
